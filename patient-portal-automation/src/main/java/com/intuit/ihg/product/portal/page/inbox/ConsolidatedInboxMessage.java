893de85056dbcff09d8dd1940895fc50e1a9dd2c
Shanthala B <Shanthala_B@intuit.com>
2013-10-09 13:29:52 +0000

    Changed Junit assert into TestNG
    [git-p4: depot-paths = "//IHG_DEPOT/trunk/qa-automation/": change = 274359]

--- patient-portal-automation/src/main/java/com/intuit/ihg/product/portal/page/inbox/ConsolidatedInboxMessage.java
+++ patient-portal-automation/src/main/java/com/intuit/ihg/product/portal/page/inbox/ConsolidatedInboxMessage.java
@@ -1,10 +1,10 @@
 package com.intuit.ihg.product.portal.page.inbox;
 
-import org.junit.Assert;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.testng.Assert;
 
 import com.intuit.ifs.csscat.core.pageobject.BasePageObject;
 import com.intuit.ihg.common.utils.IHGUtil;
