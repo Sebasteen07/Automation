367591ccc44046e355922d89f8429c23c1832fd4
Prokop Rehacek <Prokop_Rehacek@medfusion.com>
2014-02-10 13:02:26 +0000

    fixed RxRenewal mobile automation test. Added wait for element in MessageInboxPage method
    [git-p4: depot-paths = "//IHG_DEPOT/trunk/qa-automation/": change = 279490]

--- mobile-automation/src/main/java/com/intuit/ihg/product/mobile/page/solutions/inbox/MessageInboxPage.java
+++ mobile-automation/src/main/java/com/intuit/ihg/product/mobile/page/solutions/inbox/MessageInboxPage.java
@@ -34,8 +34,9 @@ public class MessageInboxPage extends MobileBasePage {
 	}
 
 	public MessageDetailsPage clickMessage(String sSubject) throws InterruptedException {
-		Thread.sleep(2000);
 		message = driver.findElement(By.xpath("//a[contains(@href,'msgDetail')]/h4[text()='" + sSubject + "']")); // 40205
+		IHGUtil.waitForElement(driver, 6, message);
+		Thread.sleep(2000);
 		message.click();
 		Thread.sleep(2000);
 		if (getHeaderText().contains("New Health Information Import"))
