91e8ae081c95da41f7efb9d54890b7e03caf8135
Adam Warzel <adam_warzel@medfusion.com>
2014-02-25 09:58:20 +0000

    US7492 - additional change in page object
    [git-p4: depot-paths = "//IHG_DEPOT/trunk/qa-automation/": change = 280376]

--- patient-portal-automation/src/main/java/com/intuit/ihg/product/portal/page/questionnaires/FormWelcomePage.java
+++ patient-portal-automation/src/main/java/com/intuit/ihg/product/portal/page/questionnaires/FormWelcomePage.java
@@ -1,9 +1,13 @@
 package com.intuit.ihg.product.portal.page.questionnaires;
 
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
 import com.intuit.ifs.csscat.core.pageobject.BasePageObject;
 import com.intuit.ihg.common.utils.IHGUtil;
 import com.intuit.ihg.product.portal.utils.PortalUtil;
@@ -17,7 +21,7 @@ public class FormWelcomePage extends BasePageObject
 		// TODO Auto-generated constructor stub
 	}
 
-	@FindBy(xpath="//a[contains(@href,'demographics')]")
+	@FindBy(id="continueWelcomePageButton")
 	private WebElement btnContinue;
 
 
@@ -29,9 +33,25 @@ public class FormWelcomePage extends BasePageObject
 	public FormBasicInfoPage clickContinueButton() throws Exception{
 		IHGUtil.PrintMethodName();
 		PortalUtil.setquestionnarieFrame(driver);
+		WebDriverWait wait = new WebDriverWait(driver, 10);
+		wait.until(ExpectedConditions.elementToBeClickable(btnContinue));
 		btnContinue.click();
 		return PageFactory.initElements(driver, FormBasicInfoPage.class);
 
 	}
 
+	/**
+	 * Checks if the Welcome page of the form is loaded
+	 * @return True if Continue button (which is the main functional part of the page) is loaded, otherwise false
+	 */
+	public boolean isWelcomePageLoaded() {
+		boolean result = false;
+		try {
+			result = btnContinue.isDisplayed();
+		} catch (NoSuchElementException e) {
+			log("Welcome page of forms not loaded");
+		}
+		return result;
+	}
+	
 }
