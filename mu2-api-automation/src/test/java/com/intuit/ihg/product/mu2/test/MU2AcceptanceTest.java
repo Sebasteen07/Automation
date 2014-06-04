7542b1f3e7420f8bf76a664d57269ff87124bcfb
Chintan Shah <cshah@medfusion.com>
2014-06-04 20:43:22 +0530

    Removed warnings

--- mu2-api-automation/src/test/java/com/intuit/ihg/product/mu2/test/MU2AcceptanceTest.java
+++ mu2-api-automation/src/test/java/com/intuit/ihg/product/mu2/test/MU2AcceptanceTest.java
@@ -7,9 +7,6 @@ import java.util.List;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.testng.Assert;
-import org.testng.ITestResult;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import com.intuit.ifs.csscat.core.BaseTestNGWebDriver;
