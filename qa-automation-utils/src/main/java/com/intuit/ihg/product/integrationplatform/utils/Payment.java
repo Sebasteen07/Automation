2d43ffab6063642dc7029103451ffc181d2beb46
Chintan Shah <chintan_shah@medfusion.com>
2014-12-30 09:52:12 +0530

    US8125 - Payments API (GET)

---
+++ qa-automation-utils/src/main/java/com/intuit/ihg/product/integrationplatform/utils/Payment.java
@@ -0,0 +1,16 @@
+package com.intuit.ihg.product.integrationplatform.utils;
+
+public class Payment {
+	
+	public String Url = "";
+	public String UserName = "";
+	public String Password = "";
+	public String RestUrl = "";
+	public String ResponsePath = "";
+	public String OAuthProperty ="";
+	public String OAuthKeyStore ="";
+	public String OAuthAppToken ="";
+	public String OAuthUsername ="";
+	public String OAuthPassword ="";
+	
+}
