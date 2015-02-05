1c57a5c6ac7e0727efdc66baef50a458de848e5a
Jakub Odvárka <jakub.odvarka@medfusion.com>
2015-02-05 17:10:10 +0100

    Revert "finalizing example strings in an effort to fix jenkins build errors (cannot find symbol? )"
    This reverts commit 4c5f68c15f6f37ffef33844f520e112fd523fe5e.

--- patient-portal-automation/src/test/java/com/intuit/ihg/product/portal/test/PortalAcceptanceTests.java
+++ patient-portal-automation/src/test/java/com/intuit/ihg/product/portal/test/PortalAcceptanceTests.java
@@ -805,11 +805,8 @@ public class PortalAcceptanceTests extends BaseTestNGWebDriver {
 		pinsuranceDetailsPage.addInsuranceDetails();
 
 		log("step 8:asserting for Insurance Name and Insurance Type");
-		final String targetClass ="add";
-		final String targetText = "Add Insurance - Click here to add an entry";
-		//button to wait for (shows when there are no insurances, we just deleted the only one)
-		//move these constants to a central place later on
-		IHGUtil.waitForElementByClassAndText(driver,targetClass,targetText,20);
+		
+		IHGUtil.waitForElementByClassAndText(driver,"add","Add Insurance - Click here to add an entry",20);
 		assertTrue(verifyTextPresent(driver, PortalConstants.InsuranceName));
 		assertTrue(verifyTextPresent(driver, PortalConstants.InsuranceType));
 
@@ -817,11 +814,7 @@ public class PortalAcceptanceTests extends BaseTestNGWebDriver {
 		pinsuranceDetailsPage.deleteInsurance();
 
 		log("step 10:asserting for Insurance Name and Insurance Type not present after Insurance deleted");
-		//button to wait for (shows when there are no insurances, we just deleted the only one)
-		//move these constants to a central place later on
-		final String targetClass2 ="submit";
-		final String targetValue = "Save Insurance";
-		IHGUtil.waitForElementByClassAndValue(driver,targetClass2,targetValue,20);
+		IHGUtil.waitForElementByClassAndValue(driver,"submit","Save Insurance",20);
 		assertFalse(verifyTextNotPresent(driver, PortalConstants.InsuranceName));
 		assertFalse(verifyTextNotPresent(driver, PortalConstants.InsuranceType));
 	}
