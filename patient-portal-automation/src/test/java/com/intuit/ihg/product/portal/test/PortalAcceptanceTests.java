b65678e091f4e1d44be7fade68d249d299efc9ee
Jakub Odvárka <jakub.odvarka@medfusion.com>
2015-02-05 16:34:54 +0100

    switching the new util methods to static (for hudson5)

--- patient-portal-automation/src/test/java/com/intuit/ihg/product/portal/test/PortalAcceptanceTests.java
+++ patient-portal-automation/src/test/java/com/intuit/ihg/product/portal/test/PortalAcceptanceTests.java
@@ -779,7 +779,6 @@ public class PortalAcceptanceTests extends BaseTestNGWebDriver {
 		log("Test Case: testAddInsurance");
 		log("Execution Environment: " + IHGUtil.getEnvironmentType());
 		log("Execution Browser: " + TestConfig.getBrowserType());
-		IHGUtil util = new IHGUtil(driver);
 
 		log("step 2: Get Data from Excel");
 		Portal portal = new Portal();
@@ -807,7 +806,7 @@ public class PortalAcceptanceTests extends BaseTestNGWebDriver {
 
 		log("step 8:asserting for Insurance Name and Insurance Type");
 		
-		util.waitForElementByClassAndText("add","Add Insurance - Click here to add an entry",20);
+		IHGUtil.waitForElementByClassAndText(driver,"add","Add Insurance - Click here to add an entry",20);
 		assertTrue(verifyTextPresent(driver, PortalConstants.InsuranceName));
 		assertTrue(verifyTextPresent(driver, PortalConstants.InsuranceType));
 
@@ -815,7 +814,7 @@ public class PortalAcceptanceTests extends BaseTestNGWebDriver {
 		pinsuranceDetailsPage.deleteInsurance();
 
 		log("step 10:asserting for Insurance Name and Insurance Type not present after Insurance deleted");
-		util.waitForElementByClassAndValue("submit","Save Insurance",20);
+		IHGUtil.waitForElementByClassAndValue(driver,"submit","Save Insurance",20);
 		assertFalse(verifyTextNotPresent(driver, PortalConstants.InsuranceName));
 		assertFalse(verifyTextNotPresent(driver, PortalConstants.InsuranceType));
 	}
