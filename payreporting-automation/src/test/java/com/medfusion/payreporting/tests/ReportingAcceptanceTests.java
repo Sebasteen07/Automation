9658db0a4dc6faa16c6c907c7172ac3854450463
Jakub Odvarka <jakub.odvarka@medfusion.com>
2015-08-03 16:21:43 +0200

    iso reporting - added a test paid from patient portal - static amount - changed refund test to submit a static amount - $18.31 (one of the values that was getting rounded in the recent defect)

--- payreporting-automation/src/test/java/com/medfusion/payreporting/tests/ReportingAcceptanceTests.java
+++ payreporting-automation/src/test/java/com/medfusion/payreporting/tests/ReportingAcceptanceTests.java
@@ -10,10 +10,14 @@ import com.intuit.ifs.csscat.core.TestConfig;
 import com.intuit.ihg.common.utils.IHGUtil;
 import com.intuit.ihg.common.utils.dataprovider.PropertyFileLoader;
 import com.intuit.ihg.common.utils.monitoring.TestStatusReporter;
+import com.intuit.ihg.product.object.maps.portal.page.MyPatientPage;
+import com.intuit.ihg.product.object.maps.portal.page.PortalLoginPage;
+import com.intuit.ihg.product.object.maps.portal.page.makePaymentpage.MakePaymentPage;
 import com.intuit.ihg.product.object.maps.practice.page.PracticeHomePage;
 import com.intuit.ihg.product.object.maps.practice.page.PracticeLoginPage;
 import com.intuit.ihg.product.object.maps.practice.page.onlinebillpay.PayMyBillOnlinePage;
 import com.intuit.ihg.product.portal.utils.PortalConstants;
+import com.intuit.ihg.product.portal.utils.PortalUtil;
 import com.intuit.ihg.product.practice.utils.PracticeConstants;
 import com.medfusion.product.object.maps.isoreporting.page.ReportingDailyReportPage;
 import com.medfusion.product.object.maps.isoreporting.page.ReportingLoginPage;
@@ -251,13 +255,9 @@ public class ReportingAcceptanceTests extends BaseTestNGWebDriver {
 
 		log("Getting Test Data");
 		PropertyFileLoader testData = new PropertyFileLoader();
-		String amount = IHGUtil.createRandomNumericString().substring(0, 3);
-		while (amount.charAt(0) == '0' && amount.charAt(0) == '0'){
-			log("Leading zeros, regenerating payment");
-			amount = IHGUtil.createRandomNumericString().substring(0, 3);
-		}
-		String formattedAmount = new StringBuffer(amount).insert(amount.length(), ".00").insert(0, "$").toString();
-		log(formattedAmount);
+		
+		String formattedAmount = "$18.31";
+		log("Static payment of " + formattedAmount);
 		int retries = 30;
 		
 		log("step 1: Login to Practice Portal");	
@@ -273,7 +273,7 @@ public class ReportingAcceptanceTests extends BaseTestNGWebDriver {
 
 		log("step 4: Set Patient Transaction Fields");
 		pPayMyBillOnlinePage.setTransactionsForOnlineBillPayProcess(testData.getLocationName(),testData.getProviderName(), testData.getBillingAccountNumber(),
-					amount, testData.getFirstName() + " " + testData.getLastName(), PortalConstants.CreditCardNumber, PortalConstants.CreditCardType);
+					"18.31", testData.getFirstName() + " " + testData.getLastName(), PortalConstants.CreditCardNumber, PortalConstants.CreditCardType);
 
 		log("step 5: Verify the Payment Confirmation text");
 		IHGUtil.setFrame(driver,PracticeConstants.frameName);
@@ -317,7 +317,7 @@ public class ReportingAcceptanceTests extends BaseTestNGWebDriver {
 		log("Details !");
 		Thread.sleep(500);
 		dailyPage.clickVoidButton();
-		log("Refund $3.14!");
+		log("Refunding static $3.14!");
 		dailyPage.fillRefundAmount(IHGUtil.formatNumber(314));		
 		Thread.sleep(500);
 		dailyPage.clickRefundSubmitButton();
@@ -336,8 +336,126 @@ public class ReportingAcceptanceTests extends BaseTestNGWebDriver {
 			
 		}					
 			
+	}	
+	
+	@Test(enabled = true, groups = { "AcceptanceTests" }, retryAnalyzer = RetryAnalyzer.class)
+	public void testPayAsPatientVerifyDailyReport() throws Exception {
+		log("Test Case: Practice -> Pay As Patient -> Verify in reporting");
+		log("Execution Environment: " + IHGUtil.getEnvironmentType());
+		log("Execution Browser: " + TestConfig.getBrowserType());
+
+		log("Getting Test Data");
+		PropertyFileLoader testData = new PropertyFileLoader();
+		//Payment uses PortalConstants.Amount (100.00)
+		String amount = "100";		
+		String formattedAmount = new StringBuffer(amount).insert(amount.length(), ".00").insert(0, "$").toString();
+		log(formattedAmount);
+		int retries = 30;
+		
+		log("step 1: Login to Patient Portal");			
+		log("USER NAME: " + testData.getUserId());
+		log("Password: " + testData.getPassword());
+
+		log("step 2: LogIn");
+		PortalLoginPage ploginPage = new PortalLoginPage(driver, testData.getUrl());
+		MyPatientPage myPatientPage = ploginPage.login(testData.getUserId(), testData.getPassword());
+		log("step 3: Verify for My Patient Page ");
+		PortalUtil.setPortalFrame(driver);
+		verifyEquals(myPatientPage.txtMyPatientPage.getText(), PortalConstants.MyPatientPage);
+
+		log("step 4: Click on Make Payment Link ");
+		MakePaymentPage makePaymentPage = myPatientPage.clickMakePaymentLnk();
+
+		log("step 5: Set Make Payments Fields");
+		makePaymentPage.setMakePaymentFields("150");
+
+		log("step 6: Logout of Patient Portal");
+		myPatientPage.logout(driver);				
+		
+		log("step 6: Log in to ISO Reporting");
+		ReportingLoginPage loginPage = new ReportingLoginPage(driver, testData.getReportingUrl());
+		ReportingDailyReportPage dailyPage = loginPage.login(testData.getDoctorLogin(), testData.getDoctorPassword());
+		
+		log("step 7: Click search and validate table");
+		dailyPage.clickSearch();
+		assertTrue(dailyPage.checkTableIntegrityOnly());
+		
+		for (int i = 0; i < retries; i++){
+			log("Validating latest submission, attempt " + (i+1) + "/" + retries);
+			if (formattedAmount.equals(dailyPage.getLastRowAmount())) {
+				log("Paid amount found! Checking table totals refresh");
+				assertTrue(dailyPage.checkTableIntegrityOnly());
+				return;	
+			}
+			else dailyPage.clickSearch();						
+			
+		}		
+		log("Latest found " +dailyPage.getLastRowAmount() + " expected " + formattedAmount +" => Failure");
+		assertTrue(false);
+		
 	}
 	
+	//TODO
+	@Test(enabled = true, groups = { "AcceptanceTests" }, retryAnalyzer = RetryAnalyzer.class)
+	public void testPayVCSVerifyDailyReport() throws Exception {
+		log("Test Case: Practice -> OBP for patient -> Verify in reporting");
+		log("Execution Environment: " + IHGUtil.getEnvironmentType());
+		log("Execution Browser: " + TestConfig.getBrowserType());
+
+		log("Getting Test Data");
+		PropertyFileLoader testData = new PropertyFileLoader();
+		String amount = IHGUtil.createRandomNumericString().substring(0, 2);
+		while (amount.charAt(0) == '0'){
+			log("Leading zero, generating single digit payment");
+			amount = IHGUtil.createRandomNumericString().substring(0, 1);
+		}
+		String formattedAmount = new StringBuffer(amount).insert(amount.length(), ".00").insert(0, "$").toString();
+		log(formattedAmount);
+		int retries = 30;
+		
+		log("step 1: Login to Practice Portal");	
+
+		//Now start login with practice data
+		PracticeLoginPage practiceLogin = new PracticeLoginPage(driver, testData.getPortalUrl());
+		PracticeHomePage pPracticeHomePage = practiceLogin.login(testData.getDoctorLogin(), testData.getDoctorPassword());
+
+		log("step 2: Click On Online BillPayment Tab in Practice Portal--->Make Payment For Patient");
+		PayMyBillOnlinePage pPayMyBillOnlinePage = pPracticeHomePage.clickMakePaymentForPatient();
+
+		log("step 3: Search For Patient");
+		pPayMyBillOnlinePage.searchForPatient(testData.getFirstName(), testData.getLastName());
+
+		log("step 4: Set Patient Transaction Fields");
+		pPayMyBillOnlinePage.setTransactionsForOnlineBillPayProcess(testData.getLocationName(),testData.getProviderName(), testData.getBillingAccountNumber(),
+					amount, testData.getFirstName() + " " + testData.getLastName(), PortalConstants.CreditCardNumber, PortalConstants.CreditCardType);
+
+		log("step 5: Verify the Payment Confirmation text");
+		IHGUtil.setFrame(driver,PracticeConstants.frameName);
+		IHGUtil.waitForElement(driver,20,pPayMyBillOnlinePage.paymentConfirmationText);
+		verifyEquals(true,pPayMyBillOnlinePage.paymentConfirmationText.getText().contains(PracticeConstants.PaymentSuccessfullText));
+		
+		log("step 6: Log in to ISO Reporting");
+		ReportingLoginPage loginPage = new ReportingLoginPage(driver, testData.getReportingUrl());
+		ReportingDailyReportPage dailyPage = loginPage.login(testData.getDoctorLogin(), testData.getDoctorPassword());
+		
+		log("step 7: Click search and validate table");
+		dailyPage.clickSearch();
+		assertTrue(dailyPage.checkTableIntegrityOnly());
+		
+		for (int i = 0; i < retries; i++){
+			log("Validating latest submission, attempt " + (i+1) + "/" + retries);
+			if (formattedAmount.equals(dailyPage.getLastRowAmount())) {
+				log("Paid amount found! Checking table totals refresh");
+				assertTrue(dailyPage.checkTableIntegrityOnly());
+				return;	
+			}
+			else dailyPage.clickSearch();						
+			
+		}		
+		log("Latest found " +dailyPage.getLastRowAmount() + " expected " + formattedAmount +" => Failure");
+		assertTrue(false);
+		
+	}
 	
 		
 }
