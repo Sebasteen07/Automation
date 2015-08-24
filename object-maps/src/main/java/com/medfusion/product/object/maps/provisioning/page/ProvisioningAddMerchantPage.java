5f853aa93da5e052a1f37ed3f2313e6d7aee0bc3
Jakub Odvarka <jakub.odvarka@medfusion.com>
2015-08-24 20:33:57 +0200

    pay provisioning - create merchant & verify details and search

--- object-maps/src/main/java/com/medfusion/product/object/maps/provisioning/page/ProvisioningAddMerchantPage.java
+++ object-maps/src/main/java/com/medfusion/product/object/maps/provisioning/page/ProvisioningAddMerchantPage.java
@@ -1,7 +1,12 @@
 package com.medfusion.product.object.maps.provisioning.page;
 
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.How;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.Select;
 
 import com.intuit.ifs.csscat.core.pageobject.BasePageObject;
 import com.intuit.ihg.common.utils.IHGUtil;
@@ -15,5 +20,107 @@ public class ProvisioningAddMerchantPage extends BasePageObject {
 		PageFactory.initElements(driver, this);
 	}
 
+	//General Merchant Info	
+	@FindBy(how = How.XPATH, using="//form[@name='addMerchant']/fieldset[1]/div[2]/div[1]/input")
+	public WebElement merchantName;
+	@FindBy(how = How.XPATH, using="//form[@name='addMerchant']/fieldset[1]/div[3]/div[1]/div[1]/div[1]/input")
+	public WebElement externalId;
+	@FindBy(how = How.XPATH, using="//form[@name='addMerchant']/fieldset[1]/div[4]/div[1]/input")
+	public WebElement vantivLitleId;
+	@FindBy(how = How.XPATH, using="//form[@name='addMerchant']/fieldset[1]/div[5]/div[1]/input")
+	public WebElement elementId;
+		
+	//Merchant Address
+	@FindBy(how = How.XPATH, using="//form[@name='addMerchant']/fieldset[2]/div[1]/div[1]/input")
+	public WebElement address1;	
+	@FindBy(how = How.XPATH, using="//form[@name='addMerchant']/fieldset[2]/div[2]/div[1]/input")
+	public WebElement address2;
+	@FindBy(how = How.XPATH, using="//form[@name='addMerchant']/fieldset[2]/div[3]/div[1]/input")
+	public WebElement city;
+	@FindBy(how = How.XPATH, using="//form[@name='addMerchant']/fieldset[2]/div[4]/div[1]/div[1]/div[1]/input")
+	public WebElement zipcode;
+	@FindBy(how = How.XPATH, using="//form[@name='addMerchant']/fieldset[2]/div[5]/div[1]/div[1]/div[1]/select")
+	public WebElement country;
+	@FindBy(how = How.XPATH, using="//form[@name='addMerchant']/fieldset[2]/div[6]/div[1]/div[1]/div[1]/select")
+	public WebElement state;
+	
+	//Remit-to Address
+	@FindBy(how = How.XPATH, using="//form[@name='addMerchant']/fieldset[3]/legend/input")
+	public WebElement remitToCheckbox;
+	
+	@FindBy(how = How.XPATH, using="//form[@name='addMerchant']/fieldset[3]/div[1]/div[1]/div[1]/input")
+	public WebElement remitMerchantName;
+	@FindBy(how = How.XPATH, using="//form[@name='addMerchant']/fieldset[3]/div[1]/div[2]/div[1]/input")
+	public WebElement remitAddress1;
+	@FindBy(how = How.XPATH, using="//form[@name='addMerchant']/fieldset[3]/div[1]/div[3]/div[1]/input")
+	public WebElement remitAddress2;
+	@FindBy(how = How.XPATH, using="//form[@name='addMerchant']/fieldset[3]/div[1]/div[4]/div[1]/input")
+	public WebElement remitCity;
+	@FindBy(how = How.XPATH, using="//form[@name='addMerchant']/fieldset[3]/div[1]/div[5]/div[1]/div[1]/div[1]/input")
+	public WebElement remitZipcode;
+	@FindBy(how = How.XPATH, using="//form[@name='addMerchant']/fieldset[3]/div[1]/div[6]/div[1]/div[1]/div[1]/select")
+	public WebElement remitCountry;
+	@FindBy(how = How.XPATH, using="//form[@name='addMerchant']/fieldset[3]/div[1]/div[7]/div[1]/div[1]/div[1]/select")
+	public WebElement remitState;
+		
+	// Cards
+	@FindBy(how = How.XPATH, using="//form[@name='addMerchant']/fieldset[4]/div[1]/div[1]/label[1]/input")
+	public WebElement visaCheckbox;
+	@FindBy(how = How.XPATH, using="//form[@name='addMerchant']/fieldset[4]/div[1]/div[1]/label[2]/input")
+	public WebElement mastercardCheckbox;
+	@FindBy(how = How.XPATH, using="//form[@name='addMerchant']/fieldset[4]/div[1]/div[1]/label[3]/input")
+	public WebElement amexCheckbox;
+	@FindBy(how = How.XPATH, using="//form[@name='addMerchant']/fieldset[4]/div[1]/div[1]/label[4]/input")
+	public WebElement discoverCheckbox;
+	@FindBy(how = How.XPATH, using="//form[@name='addMerchant']/fieldset[4]/div[1]/div[1]/label[5]/input")
+	public WebElement carecredCheckbox;
+	
+	
+	
+	@FindBy(how = How.XPATH, using="//form[@name='addMerchant']/fieldset[5]/div[1]/div[1]/button")
+	public WebElement addButton;
+	
+	
+	
+	public ProvisioningMerchantDetailPage fillAndSubmit(String merchantName, 
+			String externalId, String vantivId, String elementId, String address1, 
+			String address2, String city, String zip, String country, String state,
+			String remitMerchantName, String remitAddress1, String remitAddress2,
+			String remitCity, String remitZip, String remitCountry, String remitState,
+			boolean amex, boolean visa, boolean discover, boolean carecred){
+		this.merchantName.sendKeys(merchantName);
+		this.externalId.sendKeys(externalId);
+		vantivLitleId.sendKeys(vantivId);
+		this.elementId.sendKeys(elementId);
+		this.address1.sendKeys(address1);
+		this.address2.sendKeys(address2);
+		this.city.sendKeys(city);
+		zipcode.sendKeys(zip);
+		/*
+		Select selCountry= new Select(this.country);
+		selCountry.selectByValue(country);
+		*/
+		this.state.sendKeys(Keys.ARROW_DOWN);
+		remitToCheckbox.click();
+		this.remitMerchantName.sendKeys(remitMerchantName);
+		this.remitAddress1.sendKeys(remitAddress1);
+		this.remitAddress2.sendKeys(remitAddress2);
+		this.remitCity.sendKeys(remitCity);
+		remitZipcode.sendKeys(remitZip);	
+		this.remitState.sendKeys(Keys.ARROW_DOWN);
+		this.remitState.sendKeys(Keys.ARROW_DOWN);
+		tickCards(amex, visa, discover, carecred);
+		addButton.click();
+		return PageFactory.initElements(driver, ProvisioningMerchantDetailPage.class);		
+	}
+	
+	public void tickCards(boolean amex, boolean visa, boolean discover, boolean carecred){
+		if(amex) amexCheckbox.click();
+		if(visa) visaCheckbox.click();
+		if(discover) discoverCheckbox.click();
+		if(carecred) carecredCheckbox.click();
+	}
+	
 	
 }
+
--- object-maps/src/main/java/com/medfusion/product/object/maps/provisioning/page/ProvisioningMerchantDetailPage.java
+++ object-maps/src/main/java/com/medfusion/product/object/maps/provisioning/page/ProvisioningMerchantDetailPage.java
@@ -1,10 +1,13 @@
 package com.medfusion.product.object.maps.provisioning.page;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.How;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import com.intuit.ifs.csscat.core.pageobject.BasePageObject;
 import com.intuit.ihg.common.utils.IHGUtil;
@@ -17,6 +20,17 @@ public class ProvisioningMerchantDetailPage extends BasePageObject {
 		driver.manage().window().maximize();
 		PageFactory.initElements(driver, this);
 	}
+	//Controls
+	@FindBy(how = How.XPATH, using="//nav[@id='top-nav']//button[contains(@data-ng-click,'logout()')]")
+	public WebElement logoutButton;	
+	@FindBy(how = How.XPATH, using="//nav[@id='menu']/ul/li[3]/a")
+	public WebElement searchMerchantButton;	
+	@FindBy(how = How.XPATH, using="//nav[@id='menu']/ul/li[4]/a")
+	public WebElement addMerchantButton;	
+	@FindBy(how = How.XPATH, using="//nav[@id='menu']/ul/li[6]/a")
+	public WebElement searchUserButton;	
+	@FindBy(how = How.XPATH, using="//nav[@id='menu']/ul/li[7]/a")
+	public WebElement addUserButton;
 	
 	//General Merchant Info
 	@FindBy(how = How.XPATH, using="//div[@id='merchantDetail']/fieldset/div[1]/div[1]/div[1]/div[2]")
@@ -40,7 +54,10 @@ public class ProvisioningMerchantDetailPage extends BasePageObject {
 	@FindBy(how = How.XPATH, using="//div[@id='merchantDetail']/fieldset/div[2]/div[1]/fieldset/div[4]/div[2]")
 	public WebElement zipcode;
 	@FindBy(how = How.XPATH, using="//div[@id='merchantDetail']/fieldset/div[2]/div[1]/fieldset/div[5]/div[2]")
+	public WebElement country;
+	@FindBy(how = How.XPATH, using="//div[@id='merchantDetail']/fieldset/div[2]/div[1]/fieldset/div[6]/div[2]")
 	public WebElement state;
+	
 	
 	//Remit-to Address
 	@FindBy(how = How.XPATH, using="//div[@id='merchantDetail']/fieldset/div[2]/div[2]/fieldset/div[1]/div[2]")
@@ -54,7 +71,20 @@ public class ProvisioningMerchantDetailPage extends BasePageObject {
 	@FindBy(how = How.XPATH, using="//div[@id='merchantDetail']/fieldset/div[2]/div[2]/fieldset/div[5]/div[2]")
 	public WebElement remitZipcode;
 	@FindBy(how = How.XPATH, using="//div[@id='merchantDetail']/fieldset/div[2]/div[2]/fieldset/div[6]/div[2]")
-	public WebElement remitState;	
+	public WebElement remitCountry;	
+	@FindBy(how = How.XPATH, using="//div[@id='merchantDetail']/fieldset/div[2]/div[2]/fieldset/div[7]/div[2]")
+	public WebElement remitState;
+	
+	//Cards
+	@FindBy(how = How.XPATH, using="//img[@title='American Express']")
+	public WebElement amexImg;
+	@FindBy(how = How.XPATH, using="//img[@title='Visa']")
+	public WebElement visaImg;
+	@FindBy(how = How.XPATH, using="//img[@title='Discover']")
+	public WebElement discoverImg;
+	@FindBy(how = How.XPATH, using="//img[@title='Care Credit']")
+	public WebElement carecredImg;
+	
 	
 	//Edit merchant
 	@FindBy(how = How.XPATH, using="//div[@id='merchantDetail']/fieldset/div[@class='row actions']/div[1]/a")
@@ -68,6 +98,79 @@ public class ProvisioningMerchantDetailPage extends BasePageObject {
 			 return true;
 		else return false;	
 	}
+	public boolean verifyInfoWithoutMid(String externalId, String merchantName, 
+			String vantivLitleId, String elementId, String address1, String address2, 
+			String zipcode, String country, String state, String remitMerchantName, 
+			String remitAddress1, String remitAddress2, String remitCity, String remitZipcode, 
+			String remitCountry, String remitState){
+		IHGUtil.PrintMethodName();
+		if (externalId.equals(this.externalId.getText().trim()) 
+				&& merchantName.equals(this.merchantName.getText().trim()) 
+				&& vantivLitleId.equals(this.vantivLitleId.getText().trim())
+				&& elementId.equals(this.elementId.getText().trim())
+				&& address1.equals(this.address1.getText().trim())
+				&& address2.equals(this.address2.getText().trim())
+				&& zipcode.equals(this.zipcode.getText().trim())
+				&& country.equals(this.country.getText().trim())
+				&& state.equals(this.state.getText().trim())
+				&& remitMerchantName.equals(this.remitMerchantName.getText().trim())
+				&& remitAddress1.equals(this.remitAddress1.getText().trim())
+				&& remitAddress2.equals(this.remitAddress2.getText().trim())
+				&& remitCity.equals(this.remitCity.getText().trim())
+				&& remitZipcode.equals(this.remitZipcode.getText().trim())
+				&& remitCountry.equals(this.remitCountry.getText().trim())
+				&& remitState.equals(this.remitState.getText().trim())				
+				)
+			 return true;
+		else return false;	
+	}
 	
+	public void waitTillLoaded() {		
+		log("Loading, waiting for merchant ID element");
+		WebDriverWait wait = new WebDriverWait(driver, 5);
+		wait.until(ExpectedConditions.visibilityOfElementLocated((By.xpath("//div[@id='merchantDetail']/fieldset/div[1]/div[1]/div[1]/div[2]"))));		
+	}
+	
+	public boolean isVisaOn(){
+		try{
+			return visaImg.isDisplayed();			
+		}
+		catch (Exception e){
+			return false;
+		}
+	}
+	public boolean isAmexOn(){
+		try{
+			return amexImg.isDisplayed();			
+		}
+		catch (Exception e){
+			return false;
+		}
+	}
+	public boolean isDiscoverOn(){
+		try{
+			return discoverImg.isDisplayed();			
+		}
+		catch (Exception e){
+			return false;
+		}
+	}
+	public boolean isCarecredOn(){
+		try{
+			return carecredImg.isDisplayed();			
+		}
+		catch (Exception e){
+			return false;
+		}
+	}
+	
+	public boolean checkCards(boolean amex, boolean visa, boolean discover, boolean carecred){
+		IHGUtil.PrintMethodName();
+		if(amex) if(!isAmexOn()) return false;
+		if(visa) if(!isVisaOn()) return false;
+		if(discover) if(!isDiscoverOn()) return false;
+		if(carecred) if(!isCarecredOn()) return false;
+		return true;
+	}
 
 }
