de70459feb589bfbf66183fe83c8a5a9890d60a7
Petr Hajek <petr.hajek@medfusion.com>
2015-10-20 11:38:11 -0400

    Fix of the Precheck automation and update of all env properties

--- object-maps/src/main/java/com/medfusion/product/object/maps/precheck/page/AppointmentDetails/AppointmentDetailsPage.java
+++ object-maps/src/main/java/com/medfusion/product/object/maps/precheck/page/AppointmentDetails/AppointmentDetailsPage.java
@@ -12,58 +12,61 @@ import com.intuit.ihg.common.utils.IHGUtil;
 public class AppointmentDetailsPage extends BasePageObject {
 	
 	
-	@FindBy(how = How.XPATH, using = ".//*[@id='appointmentDate']")
+	@FindBy(how = How.ID, using = "appointmentDate")
 	public WebElement appointmentDate;
 
-	@FindBy(how = How.XPATH, using = "id_appointmentTime")
+	@FindBy(how = How.ID, using = "appointmentTime")
 	public WebElement appointmentTime;
+	
+	@FindBy(how = How.ID, using = "locationName")
+	public WebElement locationName;
 
-	@FindBy(how = How.XPATH, using = ".//*[@id='providerName']")
+	@FindBy(how = How.ID, using = "providerName")
 	public WebElement providerName;
 
-	@FindBy(how = How.XPATH, using = ".//*[@id='patientId']")
+	@FindBy(how = How.ID, using = "patientId")
 	public WebElement patientId;
 
-	@FindBy(how = How.XPATH, using = ".//*[@id='patientFirstName']")
+	@FindBy(how = How.ID, using = "patientFirstName")
 	public WebElement patientFirstName;
 
-	@FindBy(how = How.XPATH, using = ".//*[@id='patientMiddleName']")
+	@FindBy(how = How.ID, using = "patientMiddleName")
 	public WebElement patientMiddleName;
 
-	@FindBy(how = How.XPATH, using = ".//*[@id='patientLastName']")
+	@FindBy(how = How.ID, using = "patientLastName")
 	public WebElement patientLastName;
 
-	@FindBy(how = How.XPATH, using = ".//*[@id='patientDateOfBirth']")
+	@FindBy(how = How.ID, using = "patientDateOfBirth")
 	public WebElement patientDOB;
 
-	@FindBy(how = How.XPATH, using = ".//*[@id='patientMailingAddressLineOne']")
+	@FindBy(how = How.ID, using = "patientMailingAddressLineOne")
 	public WebElement patientMailingAddessLine1;
 
-	@FindBy(how = How.XPATH, using = ".//*[@id='patientMailingAddressLineTwo']")
+	@FindBy(how = How.ID, using = "patientMailingAddressLineTwo")
 	public WebElement patientMailingAddessLine2;
 
-	@FindBy(how = How.XPATH, using = ".//*[@id='patientCity']")
+	@FindBy(how = How.ID, using = "patientCity")
 	public WebElement patientCity;
 
-	@FindBy(how = How.XPATH, using = ".//*[@id='patientState']")
+	@FindBy(how = How.ID, using = "patientState")
 	public WebElement patientState;
 
-	@FindBy(how = How.XPATH, using = ".//*[@id='patientZip']")
+	@FindBy(how = How.ID, using = "patientZip")
 	public WebElement patientZip;
 
-	@FindBy(how = How.XPATH, using = ".//*[@id='patientPhone']")
+	@FindBy(how = How.ID, using = "patientPhone")
 	public WebElement patientPhoneNumber;
 
-	@FindBy(how = How.XPATH, using = ".//*[@id='patientEmail']")
+	@FindBy(how = How.ID, using = "patientEmail")
 	public WebElement patientEmail;
 
-	@FindBy(how = How.XPATH, using = ".//*[@id='patientCopayment']")
+	@FindBy(how = How.ID, using = "patientCopayment")
 	public WebElement patientCopayment;
 
-	@FindBy(how = How.XPATH, using = ".//*[@id='patientOutstandingBalance']")
+	@FindBy(how = How.ID, using = "patientOutstandingBalance")
 	public WebElement patientOutstandingBalance;
 
-	@FindBy(how = How.XPATH, using = ".//*[@id='createButton']")
+	@FindBy(how = How.ID, using = "createButton")
 	public WebElement createButton;
 	
 
@@ -75,18 +78,21 @@ public class AppointmentDetailsPage extends BasePageObject {
 		PageFactory.initElements(driver, this);
 	}
 	
-	public void scheduleAppointment(String appointmentDateS, String providerNameS, String patientIdS, String patientFirstNameS,
+	public void scheduleAppointment(String appointmentDateS, String location, String providerNameS, String patientIdS, String patientFirstNameS,
 									String patientMiddleNameS, String patientLastNameS, String patientDOBS, String patientMailingAddessLine1S,
 									String patientMailingAddessLine2S, String patientCityS, String patientZipS, String patientPhoneNumberS,
 									String patientEmailS, String patientCopaymentS, String patientOutstandingBalanceS) {
 		IHGUtil.PrintMethodName();
 		appointmentDate.sendKeys(appointmentDateS);
+		locationName.click();
+		locationName.sendKeys(location);
 		providerName.sendKeys(providerNameS);
 		patientId.sendKeys(patientIdS);
 		patientFirstName.sendKeys(patientFirstNameS);
 		patientMiddleName.sendKeys(patientMiddleNameS);
 		patientLastName.sendKeys(patientLastNameS);
 		patientDOB.sendKeys(patientDOBS);
+		patientMailingAddessLine1.click();
 		patientMailingAddessLine1.sendKeys(patientMailingAddessLine1S);
 		patientMailingAddessLine2.sendKeys(patientMailingAddessLine2S);
 		patientCity.sendKeys(patientCityS);
