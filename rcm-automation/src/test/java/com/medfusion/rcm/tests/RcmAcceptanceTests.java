a52a2332c8510d0950c6ce7d8ad0916766076b76
Jakub Odvarka <jakub.odvarka@medfusion.com>
2015-12-10 22:00:38 +0100

    rcm automation: - sftp file posting E2E test (staging sftp -> mirth -> user-svcs -> rcm -> mail&secure message -> statements) - small updates to other tests

--- rcm-automation/src/test/java/com/medfusion/rcm/tests/RcmAcceptanceTests.java
+++ rcm-automation/src/test/java/com/medfusion/rcm/tests/RcmAcceptanceTests.java
@@ -8,6 +8,7 @@ import java.util.Random;
 import java.util.Scanner;
 
 
+
 import org.junit.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -41,6 +42,22 @@ import com.medfusion.product.object.maps.jalapeno.page.HomePage.JalapenoHomePage
 import com.medfusion.product.object.maps.jalapeno.page.MessagesPage.JalapenoMessagesPage;
 import com.medfusion.rcm.utils.PatientStatementInfo;
 import com.medfusion.rcm.utils.RCMUtil;
+
+
+
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.OutputStream;
+ 
+
+import com.jcraft.jsch.Channel;
+import com.jcraft.jsch.ChannelSftp;
+import com.jcraft.jsch.JSch;
+import com.jcraft.jsch.Session;
+	
 /**
  * @Author:Jakub Odvarka
  * @Date:24.4.2015
@@ -76,24 +93,7 @@ public class RcmAcceptanceTests extends BaseTestNGWebDriver {
 		billingAccountsPractice.addHeader( "Authorization", "Basic " + doctorAuth );
 		log("Set Expected Status Code = 200");
 		billingAccountsPractice.setExpectedStatusCode( 200 );
-		assertTrue(billingAccountsPractice.get());			
-		/*		
-		log("Requesting from merchant logo");
-		merchantLogo.setServiceUrl(merchantLogoRest);		
-		merchantLogo.setContentType( "application/json;" );		
-		log("Set Expected Status Code = 200");
-		merchantLogo.setExpectedStatusCode( 200 );		
-		assertTrue(merchantLogo.get("image/jpeg"));					
-		log("Requesting from statements PDF fetch");
-		statementsPractice.setServiceUrl(statementPDFRest);		
-		statementsPractice.addHeader( "Authorization", "Basic " + doctorAuth);
-		statementsPractice.setContentType( "application/json;" );		
-		log("Set Expected Status Code = 200");
-		statementsPractice.setExpectedStatusCode( 200 );		
-		assertTrue(statementsPractice.get("application/pdf"));
-		*/	
-		
-		
+		assertTrue(billingAccountsPractice.get());							
 	}
 	
 	@Test(enabled = true, groups = { "AcceptanceTests" }, retryAnalyzer = RetryAnalyzer.class)
@@ -121,17 +121,16 @@ public class RcmAcceptanceTests extends BaseTestNGWebDriver {
 		JalapenoHomePage jalapenoHomePage = jalapenoLoginPage.login(testData.getUserId(), testData.getPassword());		
 		
 		log("Click on messages solution");
-		JalapenoMessagesPage jalapenoMessagesPage = jalapenoHomePage.showMessages(driver);
-		//this can unfortunately take a while, patient has lots of messages
-		Thread.sleep(10000);			
-		
+		JalapenoMessagesPage jalapenoMessagesPage = jalapenoHomePage.showMessages(driver);		
+		WebDriverWait wait = new WebDriverWait(driver, 20);
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@id='messageActions']")));					
 		assertTrue(jalapenoMessagesPage.assessMessagesElements());
 		
 		log("Expect an estatement message");
 		assertTrue(jalapenoMessagesPage.isMessageFromEstatementsDisplayed(driver));
 		
 		log("Archive the message");
-		jalapenoMessagesPage.archiveOpenMessage();
+		jalapenoMessagesPage.archiveOpenMessage();		
 					
 		jalapenoMessagesPage.goToPayBillsPage(driver);
 		log("Check expected balance");
@@ -140,15 +139,78 @@ public class RcmAcceptanceTests extends BaseTestNGWebDriver {
 		log("Balance checks out!");
 		
 	}
+	
+	@Test(enabled = true, groups = { "AcceptanceTests" }, retryAnalyzer = RetryAnalyzer.class)
+	public void testSendStmtE2ESftpGWMirth() throws Exception {
+
+		log(this.getClass().getName());
+		Mailinator mail = new Mailinator();
+		Session     session;
+		Channel     channel;
+		ChannelSftp channelSftp;
+		
+		log("Execution Environment: " + IHGUtil.getEnvironmentType());
+		log("Execution Browser: " + TestConfig.getBrowserType());
+
+		log("Getting Test Data");
+		PropertyFileLoader testData = new PropertyFileLoader();	
+		URL url = ClassLoader.getSystemResource("testfiles/"+ IHGUtil.getEnvironmentType().toString() + "/" + "gwayestAUTO.002");
+		
+		log("Post eStatement");				 
+		try{
+	        JSch jsch = new JSch();
+	        session = jsch.getSession(testData.getSftpUser(),testData.getSftpHost(),22);
+	        session.setPassword(testData.getSftpPassword());
+	        java.util.Properties config = new java.util.Properties();
+	        config.put("StrictHostKeyChecking", "no");
+	        session.setConfig(config);
+	        session.connect();
+	        channel = session.openChannel("sftp");
+	        channel.connect();
+	        channelSftp = (ChannelSftp)channel;		            
+	        File f = new File(url.getPath());
+	        channelSftp.put(new FileInputStream(f), f.getName());
+		}catch(Exception ex){
+			System.out.println("SFTP File Upload attempt failed!");
+			ex.printStackTrace();
+		}	
+		
+		log("Check email notification and URL");
+		String box = testData.getEmail().split("@")[0];		
+		assertTrue(mail.catchNewMessageCheckLinkUrl(box, "Your patient eStatement is now available","Visit our website",testData.getUrl(), 108));
+		
+		log("Log in");
+		JalapenoLoginPage jalapenoLoginPage = new JalapenoLoginPage(driver,testData.getUrl());
+		JalapenoHomePage jalapenoHomePage = jalapenoLoginPage.login(testData.getUserId(), testData.getPassword());		
+		
+		log("Click on messages solution");
+		JalapenoMessagesPage jalapenoMessagesPage = jalapenoHomePage.showMessages(driver);		
+		WebDriverWait wait = new WebDriverWait(driver, 20);
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@id='messageActions']")));					
+		assertTrue(jalapenoMessagesPage.assessMessagesElements());
+		
+		log("Expect an estatement message");
+		assertTrue(jalapenoMessagesPage.isMessageFromEstatementsDisplayed(driver));
+		
+		log("Archive the message");
+		jalapenoMessagesPage.archiveOpenMessage();		
+					
+		jalapenoMessagesPage.goToPayBillsPage(driver);
+		log("Check expected balance");
+		String balance  = getBalanceDue(driver);
+		assertTrue(testData.getSftpBalance().equals(balance));
+		log("Balance checks out!");
+		
+	}
 		
 	@Test(enabled = true, groups = { "AcceptanceTests" }, retryAnalyzer = RetryAnalyzer.class)
 	public void testSendDynamicElectronicFormat1() throws Exception {	    
-		testSendDynamicByFileAndPreference("statementDF1.txt", "EF1", 2);
+		sendDynamicByFileAndPreference("statementDF1.txt", "EF1", 2);
 	}
 	
 	@Test(enabled = true, groups = { "AcceptanceTests" }, retryAnalyzer = RetryAnalyzer.class)
 	public void testSendDynamicElectronicFormat2() throws Exception {	    
-		testSendDynamicByFileAndPreference("statementDF2.txt", "EF2", 2);
+		sendDynamicByFileAndPreference("statementDF2.txt", "EF2", 2);
 	}
 		
 	@Test(enabled = true, groups = { "AcceptanceTests" }, retryAnalyzer = RetryAnalyzer.class)
@@ -214,7 +276,7 @@ public class RcmAcceptanceTests extends BaseTestNGWebDriver {
 		return poster.getAndSearchForMatch("\"customerBalance\":"+balanceToFind);
 	}
 	
-	protected void testSendDynamicByFileAndPreference(String filename, String idPrefix, int preference) throws Exception {	    
+	protected void sendDynamicByFileAndPreference(String filename, String idPrefix, int preference) throws Exception {	    
 		log(this.getClass().getName());
 		log("Getting Test Data");
 		
@@ -245,23 +307,29 @@ public class RcmAcceptanceTests extends BaseTestNGWebDriver {
 		JalapenoHomePage jalapenoHomePage = jalapenoLoginPage.login(newPat.username, newPat.password);
 		
 		//check presence of badge (electronic preference &&(last payment date < newest statement date || (no payments present && statement arrived)))
-		log("No payments present, expect a badge with balance on Pay Bills");
-		String badge = driver.findElement(By.xpath("//a[@id='feature_bill_pay']/span[@class='badge amountDue ng-binding']")).getText();
-		log("Badge retrieved, expected balance: $" + newPat.balance  + " , found balance: " + badge);		
-		assertTrue(badge.trim().equals("$"+newPat.balance));					
-		
-		log("Click on messages solution");
-		JalapenoMessagesPage jalapenoMessagesPage = jalapenoHomePage.showMessages(driver);		
-		assertTrue(jalapenoMessagesPage.assessMessagesElements());
-		
-		log("Expect an estatement message");
-		assertTrue(jalapenoMessagesPage.isMessageFromEstatementsDisplayed(driver));
-		
-		log("Archive the message");
-		jalapenoMessagesPage.archiveOpenMessage();		
-		
-		log("Check expected balance");
-		jalapenoMessagesPage.goToPayBillsPage(driver);
+		if (preference == 2){
+			log("No payments present, expect a badge with balance on Pay Bills");
+			String badge = driver.findElement(By.xpath("//a[@id='feature_bill_pay']/span[@class='badge amountDue ng-binding']")).getText();
+			log("Badge retrieved, expected balance: $" + newPat.balance  + " , found balance: " + badge);		
+			assertTrue(badge.trim().equals("$"+newPat.balance));		
+			//paper patients don't receive a sm either
+			log("Click on messages solution");
+			JalapenoMessagesPage jalapenoMessagesPage = jalapenoHomePage.showMessages(driver);		
+			assertTrue(jalapenoMessagesPage.assessMessagesElements());
+			
+			log("Expect an estatement message");
+			assertTrue(jalapenoMessagesPage.isMessageFromEstatementsDisplayed(driver));
+			
+			log("Archive the message");
+			jalapenoMessagesPage.archiveOpenMessage();
+			Thread.sleep(2000);
+			log("Check expected balance");
+			jalapenoMessagesPage.goToPayBillsPage(driver);
+		}
+		else {
+			log("Badge and SM omitted, check expected balance");
+			jalapenoHomePage.clickOnPayBills(driver);
+		}
 		String balance  = getBalanceDue(driver);
 		assertTrue(("$"+newPat.balance).equals(balance));
 		
@@ -271,46 +339,7 @@ public class RcmAcceptanceTests extends BaseTestNGWebDriver {
 		assertTrue(getBillingAccountInfoComparePatientBalance(testData.getRcmBillingAccountRest(),Integer.toString(newPat.billingAccountNumber),testData.getDoctorBase64AuthString(),newPat.balance));
 		
 		log("It also checks out with practice admin!");
-	}
-	
-	protected int postModifiedStatementToPatient(String filename, String endpoint, String env, String practicePatientId, String patientBalance, String prefix) throws Exception {
-		Assert.assertNotNull( 
-				"### Endpoint is null!", 
-				endpoint);
-		IHGUtil.PrintMethodName();
-		int min = 111111;
-		int max = 999999;
-		Random rand = new Random();
-		String newBillingNumber = Integer.toString((rand.nextInt((max - min) + 1) + min));
-		String payload;
-		try {
-			System.out.println("Building statement for " + env + " , setting patient to " + practicePatientId + ", generated balance " + patientBalance + " , new account number " + newBillingNumber);					
-			URL url = ClassLoader.getSystemResource("testfiles/"+ env + "/" + filename);
-			System.out.println(url.toString());
-			Scanner scan = new Scanner(new File(url.getPath()));
-			payload = scan.useDelimiter("\\Z").next();
-			scan.close();
-			payload = payload.replaceAll("BANPLACEHOLDER", newBillingNumber);
-			payload = payload.replace("MRNPLACEHOLDER", practicePatientId);
-			payload = payload.replace("BALANCEPLACEHOLDER", patientBalance);
-			payload = payload.replace("STMTIDPLACEHOLDER", prefix + "DynPatient" + newBillingNumber);
-			System.out.println("Stmt id: AutoBAN" + newBillingNumber);
-		} catch (IOException ioe) {
-			ioe.printStackTrace();
-			return -1;
-		}		
-		WebPoster poster = new WebPoster();						
-		poster.setServiceUrl(endpoint);		
-		poster.setContentType( "application/json;" );
-		poster.addHeader( "requestId", "rcmAutomationPost" + newBillingNumber );
-		System.out.println("Using requestId: rcmAutomationPost" + newBillingNumber );
-		poster.addHeader( "Authentication-Type", "2wayssl" );
-		log("Expected Status Code = 200");
-		poster.setExpectedStatusCode( 200 );	// HTTP Status Code
-		log("send Statement to patient #####");
-		poster.postFromString(payload);	
-		return Integer.parseInt(newBillingNumber);		
-	}
+	}		
 	
 	protected PatientStatementInfo activateNewPatientPrepareForStatement(WebDriver driver, PropertyFileLoader testData, int deliveryPref) throws Exception{
 		log(this.getClass().getName());
@@ -399,5 +428,44 @@ public class RcmAcceptanceTests extends BaseTestNGWebDriver {
 				
 		PatientStatementInfo result = new PatientStatementInfo(memberId, patientActivationSearchTest.getFirstNameString(), -1, "", "", patientActivationSearchTest.getFirstNameString(), testData.getPassword(), deliveryPref);		
 		return result;
+	}
+	
+	protected int postModifiedStatementToPatient(String filename, String endpoint, String env, String practicePatientId, String patientBalance, String prefix) throws Exception {
+		Assert.assertNotNull( 
+				"### Endpoint is null!", 
+				endpoint);
+		IHGUtil.PrintMethodName();
+		int min = 111111;
+		int max = 999999;
+		Random rand = new Random();
+		String newBillingNumber = Integer.toString((rand.nextInt((max - min) + 1) + min));
+		String payload;
+		try {
+			System.out.println("Building statement for " + env + " , setting patient to " + practicePatientId + ", generated balance " + patientBalance + " , new account number " + newBillingNumber);					
+			URL url = ClassLoader.getSystemResource("testfiles/"+ env + "/" + filename);
+			System.out.println(url.toString());
+			Scanner scan = new Scanner(new File(url.getPath()));
+			payload = scan.useDelimiter("\\Z").next();
+			scan.close();
+			payload = payload.replaceAll("BANPLACEHOLDER", newBillingNumber);
+			payload = payload.replace("MRNPLACEHOLDER", practicePatientId);
+			payload = payload.replace("BALANCEPLACEHOLDER", patientBalance);
+			payload = payload.replace("STMTIDPLACEHOLDER", prefix + "DynPatient" + newBillingNumber);
+			System.out.println("Stmt id: AutoBAN" + newBillingNumber);
+		} catch (IOException ioe) {
+			ioe.printStackTrace();
+			return -1;
+		}		
+		WebPoster poster = new WebPoster();						
+		poster.setServiceUrl(endpoint);		
+		poster.setContentType( "application/json;" );
+		poster.addHeader( "requestId", "rcmAutomationPost" + newBillingNumber );
+		System.out.println("Using requestId: rcmAutomationPost" + newBillingNumber );
+		poster.addHeader( "Authentication-Type", "2wayssl" );
+		log("Expected Status Code = 200");
+		poster.setExpectedStatusCode( 200 );	// HTTP Status Code
+		log("send Statement to patient #####");
+		poster.postFromString(payload);	
+		return Integer.parseInt(newBillingNumber);		
 	}
 }
