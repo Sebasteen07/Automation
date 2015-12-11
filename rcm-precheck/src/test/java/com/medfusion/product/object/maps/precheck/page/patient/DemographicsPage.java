f178b801853d8d60d3e5e541db4da1c5584c93ef
Jakub Calabek <jcalabek@medfusion.com>
2015-12-11 14:30:07 -0500

    Adding object maps definitions for Demographics page and moving PreCheck constants and PrecheckPatient classes from qa automation utils to PreCheck project.

--- rcm-precheck/src/test/java/com/medfusion/product/object/maps/precheck/page/patient/DemographicsPage.java
+++ rcm-precheck/src/test/java/com/medfusion/product/object/maps/precheck/page/patient/DemographicsPage.java
@@ -13,9 +13,53 @@ public class DemographicsPage extends BasePageObject {
 	@FindBy(how = How.ID, using = "demographicsConfirmButton")
 	private WebElement demographicsConfirmButton;
 	
+	@FindBy(how = How.ID, using = "firstName")
+	private WebElement firstNameInput;
+	
+	@FindBy(how = How.ID, using = "middleName")
+	private WebElement middleNameInput;
+	
+	@FindBy(how = How.ID, using = "lastName")
+	private WebElement lastNameInput;
+	
+	@FindBy(how = How.ID, using = "dob")
+	private WebElement dobInput;
+	
+	@FindBy(how = How.ID, using = "street")
+	private WebElement streetInput;
+
+	@FindBy(how = How.ID, using = "street2")
+	private WebElement street2Input;
+	
+	@FindBy(how = How.ID, using = "city")
+	private WebElement cityInput;
+	
+	@FindBy(how = How.ID, using = "state")
+	private WebElement stateInput;
+	
+	@FindBy(how = How.ID, using = "zip")
+	private WebElement zipInput;
+	
+	@FindBy(how = How.ID, using = "phone")
+	private WebElement phoneInput;
+	
+	@FindBy(how = How.ID, using = "email")
+	private WebElement emailInput;
+	
+	@FindBy(how = How.ID, using = "preferredPharmacyName")
+	private WebElement preferredPharmacyNameInput;
+	
+	@FindBy(how = How.ID, using = "preferredPharmacyPhoneNumber")
+	private WebElement preferredPharmacyPhoneNumber;
+	
+	
 	public DemographicsPage(WebDriver driver) {
 		super(driver);
 		PageFactory.initElements(driver, this);
+	}
+	
+	public void fillInDemogprahicsData() {
+		
 	}
 	
 	public PatientHomePage clickConfirmDemographics() {
