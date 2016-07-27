9560a8dcea86a62254e6d017f5d88083fd246cc3
David Tilser <david.tilser@sde.cz>
2016-07-27 10:58:30 +0200

    SON-1754: Automation tests for Referrals for actived patient, deactivated patient and nonexisting patient.

---
+++ practiceportal-automation/src/test/java/com/intuit/ihg/product/practice/test/ReferralsAcceptanceTests.java
@@ -0,0 +1,80 @@
+package com.intuit.ihg.product.practice.test;
+
+import org.testng.annotations.Test;
+
+import com.intuit.ifs.csscat.core.BaseTestNGWebDriver;
+import com.intuit.ifs.csscat.core.TestConfig;
+import com.medfusion.common.utils.IHGUtil;
+import com.medfusion.common.utils.PropertyFileLoader;
+import com.medfusion.product.object.maps.practice.page.PracticeHomePage;
+import com.medfusion.product.object.maps.practice.page.PracticeLoginPage;
+import com.medfusion.product.object.maps.practice.page.referrals.ReferralsPage;
+import com.medfusion.product.practice.api.pojo.Practice;
+import com.medfusion.product.practice.api.pojo.PracticeTestData;
+import com.medfusion.product.practice.api.utils.ReadFilePath;
+
+public class ReferralsAcceptanceTests extends BaseTestNGWebDriver {
+	ReadFilePath path = new ReadFilePath();
+	PropertyFileLoader testReferralsData = new PropertyFileLoader();
+
+	public ReferralsAcceptanceTests() throws Exception {
+		path.getFilepath("documents");
+	}
+
+	/*
+	 * @Author: dtilser User Story: SON-1754
+	 * 
+	 * @Date: 07/21/2016
+	 * 
+	 * @Types of patients
+	 * 
+	 */
+
+	@Test(enabled = true, groups = { "AcceptanceTests" })
+	public void testReferralsActivePatient() throws Exception {
+		testSendReferrals(testReferralsData.getProperty("firstNameActive"),
+				testReferralsData.getProperty("lastNameActive"), testReferralsData.getProperty("practice"), true);
+	}
+
+	@Test(enabled = true, groups = { "AcceptanceTests" })
+	public void testReferralsDeactivatedPatient() throws Exception {
+		testSendReferrals(testReferralsData.getProperty("firstNameDeactivated"),
+				testReferralsData.getProperty("lastNameDeactivated"), testReferralsData.getProperty("practice"), true);
+	}
+
+	@Test(enabled = true, groups = { "AcceptanceTests" })
+	public void testReferralsNonexistingPatient() throws Exception {
+		testSendReferrals(testReferralsData.getProperty("firstNameNoExist"),
+				testReferralsData.getProperty("lastNameNoExist"), testReferralsData.getProperty("practice"), false);
+	}
+
+	private void testSendReferrals(String patientFirstName, String patientLastName, String practiceName,
+			boolean patientExists) throws Exception {
+		log("Test Case: TestSendReferrals");
+		log("Execution Environment: " + IHGUtil.getEnvironmentType());
+		log("Execution Browser: " + TestConfig.getBrowserType());
+
+		log("step 1: Login to Practice Portal");
+		Practice practice = new Practice();
+		PracticeTestData practiceTestData = new PracticeTestData(practice);
+
+		// Now start login with practice data
+		PracticeLoginPage practiceLogin = new PracticeLoginPage(driver, practiceTestData.getUrl());
+		PracticeHomePage practiceHome = practiceLogin.login(testReferralsData.getProperty("userid"),
+				testReferralsData.getProperty("password"));
+
+		log("step 2: Go into Referrals");
+		ReferralsPage referralsPage = practiceHome.clickOnReferrals();
+
+		log("step 3: Send Referral");
+		referralsPage.sendReferralToAnotherPractice(patientFirstName, patientLastName, practiceName, patientExists);
+
+		log("step 4: Go into other practice and go into Referrals");
+		practiceLogin = new PracticeLoginPage(driver, practiceTestData.getUrl());
+		practiceHome = practiceLogin.login(practiceTestData.getUsername(), practiceTestData.getPassword());
+		practiceHome.clickOnReferrals();
+
+		log("step 4: Check if referral arrived");
+		referralsPage.checkReferalArrived(patientFirstName, patientLastName);
+	}
+}
---
+++ practiceportal-object-maps/src/main/java/com/medfusion/product/object/maps/practice/page/referrals/ReferralsPage.java
@@ -0,0 +1,147 @@
+package com.medfusion.product.object.maps.practice.page.referrals;
+
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.Select;
+
+import com.intuit.ifs.csscat.core.pageobject.BasePageObject;
+import com.medfusion.common.utils.IHGUtil;
+
+public class ReferralsPage extends BasePageObject {
+	@FindBy(xpath = ".//a[contains(@href, 'rp.create')]")
+	private WebElement sendReferrals;
+
+	@FindBy(xpath = ".//a[contains(@href, 'rp.incoming')]")
+	private WebElement incomingReferrals;
+
+	@FindBy(xpath = ".//a[contains(@href, 'rp.outgoing')]")
+	private WebElement outgoingReferrals;
+
+	@FindBy(xpath = ".//a[contains(@href, 'rp.contacts')]")
+	private WebElement contacts;
+
+	@FindBy(xpath = ".//a[contains(@href, 'rp.invite')]")
+	private WebElement sendInvitation;
+
+	@FindBy(xpath = ".//a[contains(@href, 'rp.invitein')]")
+	private WebElement incomingInvitation;
+
+	@FindBy(xpath = ".//a[contains(@href, 'rp.inviteout')]")
+	private WebElement outgoingInvitation;
+
+	@FindBy(xpath = ".//a[contains(@href, 'home.main')]")
+	private WebElement home;
+
+	@FindBy(xpath = ".//input[@name='memberNameTextField']")
+	private WebElement memberNameTestField;
+
+	@FindBy(id = "id5")
+	private WebElement practiceNameTestField;
+
+	@FindBy(id = "id1c")
+	private WebElement referralType;
+
+	@FindBy(id = "id22")
+	private WebElement attachCcdCheckBox;
+
+	@FindBy(id = "id4")
+	private WebElement buttonSubmit;
+
+	@FindBy(id = "idb5")
+	private WebElement reasonText;
+
+	@FindBy(xpath = ".//input[@value='Open']")
+	private WebElement openCheckBox;
+
+	@FindBy(xpath = ".//input[@name='searchParams:2:input']")
+	private WebElement patientFirstNameSearch;
+
+	@FindBy(xpath = ".//input[@name='searchParams:3:input']")
+	private WebElement patientLastNameSearch;
+
+	@FindBy(xpath = ".//input[@name='buttons:submit']")
+	private WebElement buttonSearch;
+
+	@FindBy(xpath = "//table[@id='MfAjaxFallbackDefaultDataTable']//span[contains(text(), 'IHGQA')]")
+	private WebElement arrivedReferral;
+
+	@FindBy(xpath = ".//input[@name='buttonpanel:submit']")
+	private WebElement saveChanges;
+
+	@FindBy(xpath = "//*[@id='id3-autocomplete']/ul/li")
+	private WebElement autocompleteName;
+
+	@FindBy(xpath = "//*[@id='id5-autocomplete']/ul/li")
+	private WebElement autocompleteSurname;
+
+	@FindBy(xpath = ".//input[@value='M']")
+	private WebElement genger;
+
+	@FindBy(xpath = ".//select[@name='patientNotFoundBlock:dateOfBirth:month']")
+	private WebElement month;
+
+	@FindBy(xpath = ".//select[@name='patientNotFoundBlock:dateOfBirth:day']")
+	private WebElement day;
+
+	@FindBy(xpath = ".//select[@name='patientNotFoundBlock:dateOfBirth:year']")
+	private WebElement year;
+
+	@FindBy(xpath = ".//input[@name='patientNotFoundBlock:homePhone']")
+	private WebElement homePhone;
+
+	@FindBy(xpath = ".//input['@name=practiceProviderRadio']")
+	private WebElement practiceProviderRadio;
+
+	@FindBy(xpath = ".//div[@class='referral-form']/ol/li/span[contains(text(), '123') or contains(text(), 'no')]")
+	private WebElement fullForm;
+
+	public ReferralsPage(WebDriver driver) {
+		super(driver);
+		PageFactory.initElements(driver, this);
+	}
+
+
+	public void sendReferralToAnotherPractice(String patientFirstName, String patientLastName, String practiceName,
+			boolean patientExists)
+			throws Exception {
+		IHGUtil.waitForElement(driver, 30, sendReferrals);
+		sendReferrals.click();
+		driver.switchTo().frame("iframe");
+		memberNameTestField.sendKeys(patientLastName + ", " + patientFirstName);
+		if (patientExists != false) {
+			IHGUtil.waitForElement(driver, 30, autocompleteName);
+			autocompleteName.click();
+		} else {
+			memberNameTestField.sendKeys(Keys.TAB);
+			genger.click();
+			new Select(month).selectByIndex(4);
+			new Select(day).selectByIndex(4);
+			new Select(year).selectByIndex(4);
+			homePhone.sendKeys("1234567893");
+		}
+		practiceNameTestField.sendKeys(practiceName);
+		IHGUtil.waitForElement(driver, 30, autocompleteSurname);
+		autocompleteSurname.click();
+		referralType.sendKeys("Other");
+		IHGUtil.waitForElement(driver, 30, fullForm);
+		buttonSubmit.click();
+	}
+
+	public void checkReferalArrived(String patientFirstName, String patientLastName)
+			throws Exception {
+		IHGUtil.waitForElement(driver, 30, incomingReferrals);
+		incomingReferrals.click();
+		driver.switchTo().frame("iframe");
+		openCheckBox.click();
+		patientFirstNameSearch.sendKeys(patientFirstName);
+		patientLastNameSearch.sendKeys(patientLastName);
+		buttonSearch.click();
+		IHGUtil.waitForElement(driver, 30, arrivedReferral);
+		arrivedReferral.click();
+		IHGUtil.waitForElement(driver, 30, saveChanges);
+		saveChanges.click();
+	}
+}
