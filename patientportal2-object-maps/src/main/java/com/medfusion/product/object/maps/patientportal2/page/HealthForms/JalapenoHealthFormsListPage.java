0ffb2c10e8a5146b950cb5c05171ad8e2f8359d6
Jan Tomasek <jtomasek@medfusion.com>
2016-08-15 16:32:08 +0200

    atomatioion of PI forms with FUPs, refactoring of sitegen FUPs automation, refactoring of customFormTest

--- patientportal2-object-maps/src/main/java/com/medfusion/product/object/maps/patientportal2/page/HealthForms/JalapenoHealthFormsListPage.java
+++ patientportal2-object-maps/src/main/java/com/medfusion/product/object/maps/patientportal2/page/HealthForms/JalapenoHealthFormsListPage.java
@@ -1,11 +1,13 @@
 package com.medfusion.product.object.maps.patientportal2.page.HealthForms;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.PageFactory;
-import com.medfusion.common.utils.IHGUtil;
-import com.medfusion.product.object.maps.patientportal2.page.JalapenoPage;
 
-public class JalapenoHealthFormsListPage extends JalapenoPage {
+import com.medfusion.common.utils.IHGUtil;
+import com.medfusion.product.object.maps.patientportal2.page.MedfusionPage;
+
+public class JalapenoHealthFormsListPage extends MedfusionPage {
 
 	/**
 	 * @Author:Petr Hajek
@@ -19,4 +21,23 @@ public class JalapenoHealthFormsListPage extends JalapenoPage {
 		PageFactory.initElements(driver, this);
 	}
 
+	@Override
+	public boolean assessBasicPageElements() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	/**
+	 * automatically switches to corresponding iframe
+	 * ({@link com.medfusion.product.object.maps.patientportal2.page.JalapenoPage#JalapenoNewCustomHealthFormPage(WebDriver driver)
+	 * see customFormPage constructor})
+	 * 
+	 * @param formName
+	 * @return
+	 * @throws InterruptedException
+	 */
+	public JalapenoNewCustomHealthFormPage openForm(String formName) throws InterruptedException {
+		driver.findElement(By.xpath("//a[@title='" + formName + "']")).click();
+		return PageFactory.initElements(driver, JalapenoNewCustomHealthFormPage.class);
+	}
 }
