809ffb5ee4200e2ea18308f211a91d4b484ed2b7
Jan Jancalek <jjancalek@medfusion.com>
2016-09-01 13:10:45 +0100

    PI-1698: MedfusionPage improved

--- patientportal2-object-maps/src/main/java/com/medfusion/product/object/maps/patientportal2/page/MedfusionPage.java
+++ patientportal2-object-maps/src/main/java/com/medfusion/product/object/maps/patientportal2/page/MedfusionPage.java
@@ -20,13 +20,13 @@ import com.intuit.ifs.csscat.core.pageobject.BasePageObject;
 import com.medfusion.common.utils.IHGUtil.SupportedWebElements;
 
 /**
- * Page with general functionality. TODO Should be moved somewhere to general project later
+ * Page with general functionality. TODO Should be moved somewhere to BasePageObject later
  */
 public abstract class MedfusionPage extends BasePageObject {
 
-    public MedfusionPage(WebDriver driver) {
-        super(driver);
-    }
+	public MedfusionPage(WebDriver driver) {
+		this(driver, null);
+	}
 
 	public MedfusionPage(WebDriver driver, String url) {
 		super(driver);
@@ -38,8 +38,7 @@ public abstract class MedfusionPage extends BasePageObject {
 			driver.get(sanitizedUrl);
 		}
 
-		// there's an issue related to hudson slave's resolution 1024x768 - can't click on
-		// CreateNewPatient element
+		// there's an issue related to hudson slave's resolution 1024x768 - can't click on CreateNewPatient element
 		driver.manage().window().maximize();
 		printCookies();
 		PageFactory.initElements(driver, this);
@@ -49,10 +48,6 @@ public abstract class MedfusionPage extends BasePageObject {
 		}
 	}
 
-	/**
-	 * TODO update in IHGUtil - there is System.out.println :( Usually this is just annoying so want
-	 * to put this on debug level
-	 */
 	public void printCookies() {
 		Set<Cookie> cookies = driver.manage().getCookies();
 		log("Printing Cookies -------", Level.DEBUG);
@@ -62,6 +57,9 @@ public abstract class MedfusionPage extends BasePageObject {
 		log("--------------------------", Level.DEBUG);
 	}
 
+	/**
+	 * Will check basic elements in page constructor. Should be properly overwritten if you want to make this check
+	 */
 	public abstract boolean assessBasicPageElements();
 
 
@@ -83,93 +81,90 @@ public abstract class MedfusionPage extends BasePageObject {
 		return this.getClass().getSimpleName() + ": " + logText;
 	}
 
-    /**
-     * Returns supported web element or throws error if not supported
-     */
-    public SupportedWebElements getSupportedWebElement(WebElement element) {
-        SupportedWebElements swe;
-        if ("select".equals(element.getTagName())) {
-            swe = SupportedWebElements.SELECT;
-        } else if ("input".equals(element.getTagName())) {
-            swe = SupportedWebElements.TEXT;
-        } else {
+	/**
+	 * Returns supported web element or throws error if not supported
+	 */
+	public SupportedWebElements getSupportedWebElement(WebElement element) {
+		SupportedWebElements supportedWebElement;
+		if ("select".equals(element.getTagName())) {
+			supportedWebElement = SupportedWebElements.SELECT;
+		} else if ("input".equals(element.getTagName())) {
+			supportedWebElement = SupportedWebElements.TEXT;
+		} else {
 			throw new UnsupportedOperationException("Error when processing " + elementToString(element) + ". Element is not supported.");
 		}
 
-        log("Element evaluated as: " + swe, Level.DEBUG);
-        return swe;
-    }
+		log("Element evaluated as: " + supportedWebElement, Level.DEBUG);
+		return supportedWebElement;
+	}
 
-    /**
-     * Updates web elements values, submits form and validates submitted results
-     */
-    public boolean updateAndValidateWebElements(Map<WebElement, String> map, WebElement submitElement) {
-        updateWebElements(map);
-        clickOnElement(submitElement);
-        return validateWebElements(map);
-    }
+	/**
+	 * Updates web elements values, submits form and validates submitted results
+	 */
+	public boolean updateAndValidateWebElements(Map<WebElement, String> map, WebElement submitElement) {
+		updateWebElements(map);
+		clickOnElement(submitElement);
+		return validateWebElements(map);
+	}
 
-    /**
-     * Updates web elements values
-     */
-    public void updateWebElements(Map<WebElement, String> map) {
-        for (Entry<WebElement, String> entry : map.entrySet()) {
-            updateWebElement(entry.getKey(), entry.getValue());
-        }
-    }
+	/**
+	 * Updates web elements values
+	 */
+	public void updateWebElements(Map<WebElement, String> map) {
+		for (Entry<WebElement, String> entry : map.entrySet()) {
+			updateWebElement(entry.getKey(), entry.getValue());
+		}
+	}
 
-    /**
+	/**
 	 * Clicks on web element
 	 */
 	public void clickOnElement(WebElement element) {
 		if (element != null) {
 			log("Click on: " + elementToString(element));
 			element.click();
-        } else {
+		} else {
 			throw new UnsupportedOperationException("Error when clicking element - element is null.");
-        }
-    }
+		}
+	}
 
-    /**
-     * Updates web element value
-     * Currently works with text and select elements only
-     */
-    public void updateWebElement(WebElement element, String value) {
-        log("Updating element with id: " + element.getAttribute("id") + " to value: " + value, Level.DEBUG);
+	/**
+	 * Updates web element value Currently works with text and select elements only
+	 */
+	public void updateWebElement(WebElement element, String value) {
+		log(elementToString(element) + ": updating to value: " + value, Level.DEBUG);
 
-        SupportedWebElements swe = getSupportedWebElement(element);
+		SupportedWebElements swe = getSupportedWebElement(element);
 
 		if ((swe.equals(SupportedWebElements.TEXT)) && (element.getText() != "")) {
 			element.clear();
 		}
 
-        if (swe.equals(SupportedWebElements.SELECT)) {
-            Select select = new Select(element);
+		if (swe.equals(SupportedWebElements.SELECT)) {
+			Select select = new Select(element);
 			select.selectByVisibleText(value);
 		} else if (swe.equals(SupportedWebElements.TEXT)) {
 			element.clear();
 			element.sendKeys(value);
-        } else {
-            element.sendKeys(value);
-        }
+		}
 
 		if (!validateWebElement(element, value)) {
 			// TODO what exception should be here?
-            throw new UnsupportedOperationException("Element was not set up correctly");
-        }
-    }
+			throw new UnsupportedOperationException("Element was not set up correctly");
+		}
+	}
 
-    /**
-     * Validates that web elements has the same values as expected
-     */
-    public boolean validateWebElements(Map<WebElement, String> map) {
-        for (Entry<WebElement, String> entry : map.entrySet()) {
+	/**
+	 * Validates that web elements has the same values as expected
+	 */
+	public boolean validateWebElements(Map<WebElement, String> map) {
+		for (Entry<WebElement, String> entry : map.entrySet()) {
 			if (!validateWebElement(entry.getKey(), entry.getValue())) {
-                return false;
-            }
-        }
-        return true;
-    }
+				return false;
+			}
+		}
+		return true;
+	}
 
 	private String createValidateWebElementErrorMessage(WebElement element, String value) {
 		return elementToString(element) + " has value : " + element.getAttribute("value") + " but expected: " + value + " - ERROR";
@@ -179,36 +174,35 @@ public abstract class MedfusionPage extends BasePageObject {
 		return elementToString(element) + " has value : " + element.getAttribute("value") + " - OK";
 	}
 
-    /**
-     * Validates that web element has the same value as expected
-     * Currently works with text and select elements only
-     */
+	/**
+	 * Validates that web element has the same value as expected Currently works with text and select elements only
+	 */
 	public boolean validateWebElement(WebElement element, String value) {
 		switch (getSupportedWebElement(element)) {
-            case TEXT:
-                if (!value.equals(element.getAttribute("value"))) {
+			case TEXT:
+				if (!value.equals(element.getAttribute("value"))) {
 					log(createValidateWebElementErrorMessage(element, value));
-                    return false;
-                } else {
+					return false;
+				} else {
 					log(createValidateWebElementOkMessage(element, value));
-                }
-                break;
-            case SELECT:
-                Select select = new Select(element);
-                String selectedText = select.getFirstSelectedOption().getText();
-                if (!value.equals(selectedText)) {
+				}
+				break;
+			case SELECT:
+				Select select = new Select(element);
+				String selectedText = select.getFirstSelectedOption().getText();
+				if (!value.equals(selectedText)) {
 					log(createValidateWebElementErrorMessage(element, value));
-                    return false;
-                } else {
+					return false;
+				} else {
 					log(createValidateWebElementOkMessage(element, value));
-                }
-                log(select.getFirstSelectedOption().getText(), Level.DEBUG);
-                break;
-            default:
-                break;
-        }
-        return true;
-    }
+				}
+				log(select.getFirstSelectedOption().getText(), Level.DEBUG);
+				break;
+			default:
+				break;
+		}
+		return true;
+	}
 
 
 	public boolean assessPageElements(ArrayList<WebElement> allElements) {
@@ -220,15 +214,17 @@ public abstract class MedfusionPage extends BasePageObject {
 				log("Searching for element: " + element.toString(), Level.DEBUG);
 				try {
 					new WebDriverWait(driver, 15).until(ExpectedConditions.visibilityOf(element));
-					log("Element " + element.toString() + " : is displayed", Level.DEBUG);
+					log(elementToString(element) + " : is displayed", Level.DEBUG);
 					attempt = 3;
 				} catch (StaleElementReferenceException ex) {
 					log("StaleElementReferenceException was catched, attempt: " + attempt++, Level.DEBUG);
 				} catch (TimeoutException ex) {
 					log(ex.getMessage());
+					log(elementToString(element) + " was not assessed successfuly due to TimeoutException", Level.ERROR);
 					return false;
 				} catch (Exception ex) {
 					ex.printStackTrace();
+					log(elementToString(element) + " was not assessed successfuly due to some Exception", Level.ERROR);
 					return false;
 				}
 			}
