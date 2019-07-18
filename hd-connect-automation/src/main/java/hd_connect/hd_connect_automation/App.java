02c74cc7ebea10883d90d3c28a1947fc30b8d791
Sahil Mehta <smehta@medfusion.com>
2019-07-18 14:51:53 -0400

    QA-51 HD Connect - UI Automation, folder added

---
+++ hd-connect-automation/src/main/java/hd_connect/hd_connect_automation/App.java
@@ -0,0 +1,13 @@
+package hd_connect.hd_connect_automation;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
