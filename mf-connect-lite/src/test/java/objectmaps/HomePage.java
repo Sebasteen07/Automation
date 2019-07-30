d50c76bf14357138ae702f3bd1f5b560222f4993
Vincent Enierga <venierga@medfusion.com>
2019-07-30 13:03:50 -0400

    added QA-56

--- mf-connect-lite/src/test/java/objectmaps/HomePage.java
+++ mf-connect-lite/src/test/java/objectmaps/HomePage.java
@@ -37,4 +37,98 @@ public class HomePage extends BasePageObject {
 	@FindBy(how = How.ID, using= "mfConnectBtn")
 	public WebElement mfConnectBtn;
 
+	// preselect portal area
+	@FindBy(how = How.ID, using= "portal-preselect-check")
+	public WebElement portalPreselectCheck;
+	
+	@FindBy(how = How.XPATH, using= "/html[1]/body[1]/div[1]/div[3]/form[1]/table[1]/tbody[1]/tr[2]/td[1]/input[1]")
+	public WebElement portalId;
+	
+	@FindBy(how = How.XPATH, using= "/html[1]/body[1]/div[1]/div[3]/form[1]/table[1]/tbody[1]/tr[2]/td[2]/input[1]")
+	public WebElement preselectDirLocCheck;
+	
+	@FindBy(how = How.XPATH, using= "/html[1]/body[1]/div[1]/div[3]/form[1]/table[1]/tbody[1]/tr[2]/td[3]/input[1]")
+	public WebElement preDirLocId;
+	
+	@FindBy(how = How.XPATH, using= "/html[1]/body[1]/div[1]/div[3]/form[1]/table[1]/tbody[1]/tr[2]/td[4]/input[1]")
+	public WebElement preDirLocType;
+	
+	// recommended portals area
+	@FindBy(how = How.ID, using= "recommended-portals-check")
+	public WebElement recPortalCheck;
+	
+	@FindBy(how = How.XPATH, using= "/html[1]/body[1]/div[1]/div[4]/form[1]/table[1]/tbody[1]/tr[2]/td[2]/input[1]")
+	public WebElement recEnableCheck1;
+	
+	@FindBy(how = How.XPATH, using= "/html[1]/body[1]/div[1]/div[4]/form[1]/table[1]/tbody[1]/tr[2]/td[3]/input[1]")
+	public WebElement recPortalId1;
+	
+	@FindBy(how = How.XPATH, using= "/html[1]/body[1]/div[1]/div[4]/form[1]/table[1]/tbody[1]/tr[2]/td[4]/input[1]")
+	public WebElement recDirLoc1;
+	
+	@FindBy(how = How.XPATH, using= "/html[1]/body[1]/div[1]/div[4]/form[1]/table[1]/tbody[1]/tr[2]/td[5]/input[1]")
+	public WebElement recDirLocId1;
+	
+	@FindBy(how = How.XPATH, using= "/html[1]/body[1]/div[1]/div[4]/form[1]/table[1]/tbody[1]/tr[2]/td[6]/input[1]")
+	public WebElement recDirLocType1;
+	
+	@FindBy(how = How.XPATH, using= "/html[1]/body[1]/div[1]/div[4]/form[1]/table[1]/tbody[1]/tr[3]/td[2]/input[1]")
+	public WebElement recEnableCheck2;
+	
+	@FindBy(how = How.XPATH, using= "/html[1]/body[1]/div[1]/div[4]/form[1]/table[1]/tbody[1]/tr[3]/td[3]/input[1]")
+	public WebElement recPortalId2;
+	
+	@FindBy(how = How.XPATH, using= "/html[1]/body[1]/div[1]/div[4]/form[1]/table[1]/tbody[1]/tr[3]/td[4]/input[1]")
+	public WebElement recDirLoc2;
+	
+	@FindBy(how = How.XPATH, using= "/html[1]/body[1]/div[1]/div[4]/form[1]/table[1]/tbody[1]/tr[3]/td[5]/input[1]")
+	public WebElement recDirLocId2;
+	
+	@FindBy(how = How.XPATH, using= "/html[1]/body[1]/div[1]/div[4]/form[1]/table[1]/tbody[1]/tr[3]/td[6]/input[1]")
+	public WebElement recDirLocType2;
+	
+	@FindBy(how = How.XPATH, using= "/html[1]/body[1]/div[1]/div[4]/form[1]/table[1]/tbody[1]/tr[4]/td[2]/input[1]")
+	public WebElement recEnableCheck3;
+	
+	@FindBy(how = How.XPATH, using= "/html[1]/body[1]/div[1]/div[4]/form[1]/table[1]/tbody[1]/tr[4]/td[3]/input[1]")
+	public WebElement recPortalId3;
+	
+	@FindBy(how = How.XPATH, using= "/html[1]/body[1]/div[1]/div[4]/form[1]/table[1]/tbody[1]/tr[4]/td[4]/input[1]")
+	public WebElement recDirLoc3;
+	
+	@FindBy(how = How.XPATH, using= "/html[1]/body[1]/div[1]/div[4]/form[1]/table[1]/tbody[1]/tr[4]/td[5]/input[1]")
+	public WebElement recDirLocId3;
+	
+	@FindBy(how = How.XPATH, using= "/html[1]/body[1]/div[1]/div[4]/form[1]/table[1]/tbody[1]/tr[4]/td[6]/input[1]")
+	public WebElement recDirLocType3;
+	
+	@FindBy(how = How.XPATH, using= "/html[1]/body[1]/div[1]/div[4]/form[1]/table[1]/tbody[1]/tr[5]/td[2]/input[1]")
+	public WebElement recEnableCheck4;
+	
+	@FindBy(how = How.XPATH, using= "/html[1]/body[1]/div[1]/div[4]/form[1]/table[1]/tbody[1]/tr[5]/td[3]/input[1]")
+	public WebElement recPortalId4;
+	
+	@FindBy(how = How.XPATH, using= "/html[1]/body[1]/div[1]/div[4]/form[1]/table[1]/tbody[1]/tr[5]/td[4]/input[1]")
+	public WebElement recDirLoc4;
+	
+	@FindBy(how = How.XPATH, using= "/html[1]/body[1]/div[1]/div[4]/form[1]/table[1]/tbody[1]/tr[5]/td[5]/input[1]")
+	public WebElement recDirLocId4;
+	
+	@FindBy(how = How.XPATH, using= "/html[1]/body[1]/div[1]/div[4]/form[1]/table[1]/tbody[1]/tr[5]/td[6]/input[1]")
+	public WebElement recDirLocType4;
+	
+	@FindBy(how = How.XPATH, using= "/html[1]/body[1]/div[1]/div[4]/form[1]/table[1]/tbody[1]/tr[6]/td[2]/input[1]")
+	public WebElement recEnableCheck5;
+	
+	@FindBy(how = How.XPATH, using= "/html[1]/body[1]/div[1]/div[4]/form[1]/table[1]/tbody[1]/tr[6]/td[3]/input[1]")
+	public WebElement recPortalId5;
+	
+	@FindBy(how = How.XPATH, using= "/html[1]/body[1]/div[1]/div[4]/form[1]/table[1]/tbody[1]/tr[6]/td[4]/input[1]")
+	public WebElement recDirLoc5;
+	
+	@FindBy(how = How.XPATH, using= "/html[1]/body[1]/div[1]/div[4]/form[1]/table[1]/tbody[1]/tr[6]/td[5]/input[1]")
+	public WebElement recDirLocId5;
+	
+	@FindBy(how = How.XPATH, using= "/html[1]/body[1]/div[1]/div[4]/form[1]/table[1]/tbody[1]/tr[6]/td[6]/input[1]")
+	public WebElement recDirLocType5;
 }
