79f192c13bd71a47d39b118a94447dec9a9c1dba
Vincent Enierga <venierga@medfusion.com>
2019-08-07 09:39:24 -0400

    Cleaned up long xpaths in tests

--- mf-connect-lite/src/test/java/objectmaps/SelectPortalPage.java
+++ mf-connect-lite/src/test/java/objectmaps/SelectPortalPage.java
@@ -1,19 +1,20 @@
 package objectmaps;
 
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.How;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.WebDriver;
 
-public class OfficeSelectPage extends HomePage {
 
-	public OfficeSelectPage(WebDriver driver) {
+public class SelectPortalPage extends HomePage {
+	
+	public SelectPortalPage(WebDriver driver) {
 		super(driver);
 		PageFactory.initElements(driver, this);
 	}
-	
-	@FindBy(how = How.LINK_TEXT, using = "Back to search results")
+
+	@FindBy(how = How.ID, using = "mfConnectBack")
 	public WebElement mfConnectBack;
 	
 	@FindBy(how = How.ID, using = "mfCloseButton")
@@ -21,4 +22,5 @@ public class OfficeSelectPage extends HomePage {
 	
 	@FindBy(how = How.XPATH, using = "/html[1]/body[1]/div[2]/div[2]/div[1]/div[1]/div[3]/div[1]/div[1]/ul[1]/li[1]")
 	public WebElement firstElement;
+	
 }
