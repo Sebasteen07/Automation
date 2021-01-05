09ce7a9c28be40919ae5b19831e8296bb77c98f9
ATUL_R-LP\AtulR <atul.rathod@crossasyst.com>
2021-01-05 12:34:03 +0530

    PSS-17160-2 : Missing Link Tab class added

---
+++ pss/pss2admin-objects-maps/src/main/java/com/medfusion/product/object/maps/pss2/page/settings/LinkTab.java
@@ -0,0 +1,112 @@
+// Copyright 2018-2020 NXGN Management, LLC. All Rights Reserved.
+package com.medfusion.product.object.maps.pss2.page.settings;
+
+import java.awt.HeadlessException;
+import java.awt.Toolkit;
+import java.awt.datatransfer.DataFlavor;
+import java.awt.datatransfer.UnsupportedFlavorException;
+import java.io.IOException;
+import java.util.List;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindAll;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.How;
+import org.openqa.selenium.support.ui.Select;
+
+public class LinkTab extends SettingsTab {
+
+	@FindBy(how = How.XPATH, using = "//*[@id='links']/div/div/legend/strong/a/i")
+	private WebElement linkplusButton;
+
+	@FindBy(how = How.XPATH, using = "//input[@id='search-link']")
+	private WebElement searchLink;
+
+	@FindBy(how = How.XPATH, using = "//*[@class='form-group col-lg-3 col-xs-6']/div")
+	private WebElement clickdropType;
+
+	@FindBy(how = How.XPATH, using = "//select[@name='logintype']")
+	private WebElement typeselect;
+
+	@FindBy(how = How.XPATH, using = "//*[@id='links']/div[1]/div[2]/div[2]/ng-multiselect-dropdown/div/div[1]/span/span[2]/span")
+	private WebElement locationselect;
+
+	@FindBy(how = How.XPATH, using = "//*[@id='links']/div[1]/div[2]/div[3]/ng-multiselect-dropdown/div/div[1]/span/span[2]/span")
+	private WebElement resourceselect;
+
+	@FindBy(how = How.XPATH, using = "//*[@id='links']/div[1]/div[3]/fieldset/div[1]/button")
+	private WebElement createLinkButton;
+
+	@FindBy(how = How.XPATH, using = "//a[contains(text(),'100')]")
+	private WebElement lastpage100;
+
+	@FindAll({@FindBy(xpath = "//*[@class='multiselect-item-checkbox']")})
+	private List<WebElement> checklistLocation;
+
+	@FindBy(how = How.XPATH, using = "//tbody/tr/td[5]/a[1]/i[1]")
+	private WebElement removelink;
+
+	@FindBy(how = How.XPATH, using = "//*[@class='fa fa-link']")
+	private WebElement copyLink;
+
+	public LinkTab(WebDriver driver) {
+		super(driver);
+	}
+
+	public void searchLinkandRemove(String provider) {
+		searchLink.sendKeys(provider);
+		log("Successfully Search link");
+		removelink.click();
+	}
+
+	public void searchLink(String provider) throws InterruptedException {
+		log("going to search link agin");
+		Thread.sleep(3000);
+		searchLink.clear();
+		Thread.sleep(3000);
+		searchLink.sendKeys(provider);
+	}
+
+	public void addLink(String locationConfig, String providerConfig) throws InterruptedException {
+		linkplusButton.click();
+		log("Clicked on link plus button");
+		clickdropType.click();
+		log("Clicked on type  button");
+		Select type = new Select(typeselect);
+		type.selectByVisibleText("LOGINLESS");
+		locationselect.click();
+		log("LocationTypeList " + checklistLocation.size());
+		for (int i = 0; i < checklistLocation.size(); i++) {
+			if (checklistLocation.get(i).getText().contains(locationConfig)) {
+				checklistLocation.get(i).click();
+				log("Location checkbox selected");
+			}
+		}
+		Thread.sleep(3000);
+		resourceselect.click();
+		log("ProviderTypeList " + checklistLocation.size());
+		for (int i = 0; i < checklistLocation.size(); i++) {
+			if (checklistLocation.get(i).getText().contains(providerConfig)) {
+				checklistLocation.get(i).click();
+				log("Provider checkbox selected");
+
+			}
+		}
+		createLinkButton.click();
+		log("Clicked on Create link Button");
+	}
+
+	public String getURL(String provider) throws InterruptedException, HeadlessException, UnsupportedFlavorException, IOException {
+		searchLink(provider);
+		copyLink.click();
+		String link = (String) Toolkit.getDefaultToolkit().getSystemClipboard().getData(DataFlavor.stringFlavor);
+		log("Link is   " + link);
+		return link;
+
+	}
+
+	public void removelink() {
+		lastpage100.click();
+	}
+}
