ffbdd3a42fcec3dfe290f7d2bb40eee1058f600b
Gaurav Kumar <gaurav.kumar@healthasyst.com>
2021-02-15 14:11:27 +0530

    Added existing method in jalapenoMessagesPage

--- patientportal2-object-maps/src/main/java/com/medfusion/product/object/maps/patientportal2/page/MessagesPage/JalapenoMessagesPage.java
+++ patientportal2-object-maps/src/main/java/com/medfusion/product/object/maps/patientportal2/page/MessagesPage/JalapenoMessagesPage.java
@@ -369,6 +369,38 @@ public class JalapenoMessagesPage extends JalapenoMenu {
 			log("Sender Name is correct");
 			return status;
 	}
+	public void verifyPrescriptionResponse(WebDriver driver, String prescritonRenewalResponse) {
+		IHGUtil.PrintMethodName();
+		WebElement element;
+		Boolean status= false;
+			try {
+				element = driver.findElement(By.xpath("//*[contains(text(),'" + prescritonRenewalResponse + "')]"));
+				Thread.sleep(5000);
+				if(element.isDisplayed()){
+					status= true;
+				    log("PrescritonRenewalResponse added by Practice is displayed in Portal");}
+			} catch (Exception ex) {
+				log("PrescritonRenewalResponse added by Practice is not displayed in Portal");
+				log(ex.getMessage());
+				Assert.assertTrue(status, "PrescritonRenewalResponse added by Practice is not displayed in Portal");
+			}
+	}
+	
+	public void verifyMedicationStatus(WebDriver driver, String renewalStatus) {
+		IHGUtil.PrintMethodName();
+		WebElement element;
+		Boolean status= false;
+			try {
+				element = driver.findElement(By.xpath("//*[contains(text(),'" + renewalStatus + "')]"));
+				Thread.sleep(5000);
+				if(element.isDisplayed()){
+					status= true;
+				    log("Medication status is "+renewalStatus);}
+			} catch (Exception ex) {
+				log("Medication status is not displayed in Prescription renewal message");
+				log(ex.getMessage());
+				Assert.assertTrue(status, "Medication status is not displayed in Prescription renewal message");
+			}}
 
 	public void clickOnUnArchive() throws InterruptedException {
 			log("Clicking on the unarchive from Archive Tab");
