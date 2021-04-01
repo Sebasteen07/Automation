c7bba474d0f3ce51c3aefe322199826350459dcd
CLINITEC\JRJohn <jrjohn@nextgen.com>
2021-03-31 22:04:50 -0400

    Pull Request #191 - Payment Modulator tests

---
+++ payprovisioning-automation/payment-modulator/src/test/java/com/medfusion/gateway_proxy/helpers/GatewayProxyTransactionResource.java
@@ -0,0 +1,43 @@
+// Copyright 2013-2021 NXGN Management, LLC. All Rights Reserved.
+package com.medfusion.gateway_proxy.helpers;
+
+import static io.restassured.RestAssured.given;
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
+import java.io.IOException;
+import java.util.Map;
+
+import com.medfusion.common.utils.PropertyFileLoader;
+import com.medfusion.gateway_proxy.tests.GatewayProxyBaseTest;
+import com.medfusion.payment_modulator.pojos.PayloadDetails;
+import com.medfusion.payment_modulator.utils.CommonUtils;
+import com.medfusion.payment_modulator.utils.Validations;
+
+public class GatewayProxyTransactionResource extends GatewayProxyBaseTest{
+	
+	protected PropertyFileLoader testData;
+	  
+	public void makeASale(String mmid) throws IOException {
+		  testData = new PropertyFileLoader();
+		  Map<String, Object> transactiondetails = PayloadDetails.getPayloadForAuthorizeSaleMap((testData.getProperty("transactionamount")),
+				  testData.getProperty("accountnumber"), testData.getProperty("consumername"), testData.getProperty("paymentsource"),
+				  testData.getProperty("cvv"), testData.getProperty("type"), testData.getProperty("cardnumber"),
+				  testData.getProperty("expirationnumber"), testData.getProperty("bin"), testData.getProperty("zipcode"),
+				  testData.getProperty("lastname"),testData.getProperty("addressline1"),testData.getProperty("city"),
+				  testData.getProperty("state"),testData.getProperty("firstname"));
+
+		  Response response = given().spec(requestSpec).
+		  body(transactiondetails).when().post(testData.getProperty("testpaycustomeruuid")+
+				  "/merchant/"+testData.getProperty("proxymmid")+"/sale")
+		 .then().spec(responseSpec).and().extract().response();
+			
+		  JsonPath jsonpath = new JsonPath(response.asString());
+		  Validations validate = new Validations();
+		  validate.verifyTransactionDetails(response.asString());
+		  CommonUtils.saveTransactionDetails(jsonpath.get("externalTransactionId").toString(),jsonpath.get("orderId").toString());
+
+			  
+	  }
+
+
+}
