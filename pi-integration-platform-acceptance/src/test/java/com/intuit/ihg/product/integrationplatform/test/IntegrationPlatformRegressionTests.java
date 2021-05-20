b38d7fc4c29647f27692ec2fb0d78f3093c0a42a
Sampada Jadhav <sampada.jadhav@crossasyst.com>
2021-05-20 11:46:37 +0530

    Updated Test case description

--- pi-integration-platform-acceptance/src/test/java/com/intuit/ihg/product/integrationplatform/test/IntegrationPlatformRegressionTests.java
+++ pi-integration-platform-acceptance/src/test/java/com/intuit/ihg/product/integrationplatform/test/IntegrationPlatformRegressionTests.java
@@ -4513,7 +4513,7 @@ public class IntegrationPlatformRegressionTests extends BaseTestNGWebDriver {
 		@Test(enabled = true, dataProvider = "portalVersion", groups = { "RegressionTests" }, retryAnalyzer = RetryAnalyzer.class)	
 		
 		public void testPIDCPatientRegistrationJSONV3(String portalVersion) throws Exception {
-		log("Test Case: PIDC Patient Registration for V3 channel using JSON Payload");
+		log("Test Case: Test to validate the patient Invite E2E workflow with the JSON payload for V3 endpoint");
 		log("Execution Environment: " + IHGUtil.getEnvironmentType());
 		log("Execution Browser: " + TestConfig.getBrowserType());
 		PatientRegistrationUtils.pidcPatientRegistrationJSONPayload("v3", driver, portalVersion);
