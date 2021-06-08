f5290d47b2fec30842df369a879e0843ea9ecfb9
MBush <mbush@nextgen.com>
2021-06-08 11:31:58 -0400

    QA-88 - Removing unused classes and patientportal2-service

--- patientportal2-object-maps/src/main/java/com/medfusion/product/object/maps/patientportal2/page/util/CreatePatient.java
+++ patientportal2-object-maps/src/main/java/com/medfusion/product/object/maps/patientportal2/page/util/CreatePatient.java
@@ -1,5 +1,5 @@
 //Copyright 2013-2021 NXGN Management, LLC. All Rights Reserved.
-package com.medfusion.product.patientportal2.implementedExternals;
+package com.medfusion.product.object.maps.patientportal2.page.util;
 
 import com.medfusion.pojos.Patient;
 import com.medfusion.product.object.maps.patientportal2.page.CreateAccount.PatientDemographicPage;
