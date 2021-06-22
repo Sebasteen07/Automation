1ff16c6fcabcff1097c17140de884f1a9bd39995
LAPTOP-QG05SUGH\Ankush <ankush.nagpure@crossasyst.com>
2021-06-22 16:34:02 +0530

    ISORPT-2594_get details of card

--- payprovisioning-automation/payment-modulator/src/test/java/com/medfusion/digital_wallet/tests/DigitalWalletTests.java
+++ payprovisioning-automation/payment-modulator/src/test/java/com/medfusion/digital_wallet/tests/DigitalWalletTests.java
@@ -16,58 +16,95 @@ import org.testng.annotations.Test;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
-public class DigitalWalletTests extends DigitalWalletBaseTest{
+public class DigitalWalletTests extends DigitalWalletBaseTest {
 
-    protected static PropertyFileLoader testData;
+	protected static PropertyFileLoader testData;
 
-    @BeforeTest
-    public void setUp() throws Exception{
-        testData = new PropertyFileLoader();
-        setupRequestSpecBuilder();
-    }
+	@BeforeTest
+	public void setUp() throws Exception {
+		testData = new PropertyFileLoader();
+		setupRequestSpecBuilder();
+	}
 
-    @Test
-    public void getCountOfExpiringCards() throws Throwable {
-        String token = DigitalWalletAPIUtils.getTokenForCustomer();
-        DigitalWalletResource digitalWallet = new DigitalWalletResource();
-        String fromMonth = (testData.getProperty("from.month")).substring(5,7) + (testData.getProperty("from.month")).substring(2,4);
-        String toMonth = (testData.getProperty("to.month")).substring(5,7) + (testData.getProperty("to.month")).substring(2,4);
-        Response response = digitalWallet.getCountOfExpiringCards(token, fromMonth, toMonth);
-        Assert.assertTrue(response.getStatusCode() == 200);
+	@Test
+	public void getCountOfExpiringCards() throws Throwable {
+		String token = DigitalWalletAPIUtils.getTokenForCustomer();
+		DigitalWalletResource digitalWallet = new DigitalWalletResource();
+		String fromMonth = (testData.getProperty("from.month")).substring(5, 7)
+				+ (testData.getProperty("from.month")).substring(2, 4);
+		String toMonth = (testData.getProperty("to.month")).substring(5, 7)
+				+ (testData.getProperty("to.month")).substring(2, 4);
+		Response response = digitalWallet.getCountOfExpiringCards(token, fromMonth, toMonth);
+		Assert.assertTrue(response.getStatusCode() == 200);
 
-        JsonPath jsonPath = new JsonPath(response.asString());
-        String totalCardsInDb = DBUtils.executeQueryOnDB("pay_walt","SELECT COUNT(wallet_id) FROM public.wallet_card;");
-        String totalCardsExpiringInGivenDuration = DBUtils.executeQueryOnDB("pay_walt","SELECT COUNT(wallet_id) FROM public.wallet_card where card_expiry_dt >= '"+testData.getProperty("from.month")+"' and card_expiry_dt <= '"+testData.getProperty("to.month")+"';");
+		JsonPath jsonPath = new JsonPath(response.asString());
+		String totalCardsInDb = DBUtils.executeQueryOnDB("pay_walt",
+				"SELECT COUNT(wallet_id) FROM public.wallet_card;");
+		String totalCardsExpiringInGivenDuration = DBUtils.executeQueryOnDB("pay_walt",
+				"SELECT COUNT(wallet_id) FROM public.wallet_card where card_expiry_dt >= '"
+						+ testData.getProperty("from.month") + "' and card_expiry_dt <= '"
+						+ testData.getProperty("to.month") + "';");
 
-        Assert.assertNotNull(jsonPath.get("totalCardsInSystem"),"totalCardsInSystem is null");
-        Assert.assertEquals(Integer.parseInt(totalCardsInDb), jsonPath.get("totalCardsInSystem"));
-        Assert.assertEquals(Integer.parseInt(totalCardsExpiringInGivenDuration), jsonPath.get("totalCardsExpiringInPeriod"));
+		Assert.assertNotNull(jsonPath.get("totalCardsInSystem"), "totalCardsInSystem is null");
+		Assert.assertEquals(Integer.parseInt(totalCardsInDb), jsonPath.get("totalCardsInSystem"));
+		Assert.assertEquals(Integer.parseInt(totalCardsExpiringInGivenDuration),
+				jsonPath.get("totalCardsExpiringInPeriod"));
 
-    }
+	}
 
-    @Test
-    public void getCountOfExpiringCardsWithInvalidAuth() throws Throwable {
-        String token = DigitalWalletAPIUtils.getTokenForCustomer()+"hkfd";
-        DigitalWalletResource digitalWallet = new DigitalWalletResource();
-        String fromMonth = (testData.getProperty("from.month")).substring(5,7) + (testData.getProperty("from.month")).substring(2,4);
-        String toMonth = (testData.getProperty("to.month")).substring(5,7) + (testData.getProperty("to.month")).substring(2,4);
-        Response response = digitalWallet.getCountOfExpiringCards(token, fromMonth, toMonth);
+	@Test
+	public void getCountOfExpiringCardsWithInvalidAuth() throws Throwable {
+		String token = DigitalWalletAPIUtils.getTokenForCustomer() + "hkfd";
+		DigitalWalletResource digitalWallet = new DigitalWalletResource();
+		String fromMonth = (testData.getProperty("from.month")).substring(5, 7)
+				+ (testData.getProperty("from.month")).substring(2, 4);
+		String toMonth = (testData.getProperty("to.month")).substring(5, 7)
+				+ (testData.getProperty("to.month")).substring(2, 4);
+		Response response = digitalWallet.getCountOfExpiringCards(token, fromMonth, toMonth);
 
-        Assert.assertTrue(response.getStatusCode() == 401);
-    }
+		Assert.assertTrue(response.getStatusCode() == 401);
+	}
 
-    @Test
-    public void getCountOfExpiringCardsWithInvalidDate() throws Throwable {
-        String token = DigitalWalletAPIUtils.getTokenForCustomer();
-        DigitalWalletResource digitalWallet = new DigitalWalletResource();
-        String fromMonth = (testData.getProperty("from.month.older"));
-        String toMonth = (testData.getProperty("to.month.older"));
-        Response response = digitalWallet.getCountOfExpiringCards(token, fromMonth, toMonth);
+	@Test
+	public void getCountOfExpiringCardsWithInvalidDate() throws Throwable {
+		String token = DigitalWalletAPIUtils.getTokenForCustomer();
+		DigitalWalletResource digitalWallet = new DigitalWalletResource();
+		String fromMonth = (testData.getProperty("from.month.older"));
+		String toMonth = (testData.getProperty("to.month.older"));
+		Response response = digitalWallet.getCountOfExpiringCards(token, fromMonth, toMonth);
 
-        Assert.assertTrue(response.getStatusCode() == 400);
+		Assert.assertTrue(response.getStatusCode() == 400);
 
-        JsonPath jsonPath = new JsonPath(response.asString());
-        Assert.assertEquals("Bad Request", jsonPath.get("error"));
-        Assert.assertTrue(!jsonPath.get("message").toString().isEmpty());
-    }
+		JsonPath jsonPath = new JsonPath(response.asString());
+		Assert.assertEquals("Bad Request", jsonPath.get("error"));
+		Assert.assertTrue(!jsonPath.get("message").toString().isEmpty());
+	}
+
+	@Test
+	public void getDetailsOfCardsWithValidAuth() throws Exception {
+		String token = DigitalWalletAPIUtils.getTokenForCustomer();
+
+		DigitalWalletResource digitalWallet = new DigitalWalletResource();
+		Response response = digitalWallet.getDetailsOfCards(token);
+		JsonPath jsonPath = new JsonPath(response.asString());
+		Assert.assertTrue(response.getStatusCode() == 200);
+
+		Assert.assertTrue(!jsonPath.get("externalCardId").toString().isEmpty());
+		Assert.assertTrue(!jsonPath.get("cardExpiryDate").toString().isEmpty());
+		Assert.assertTrue(!jsonPath.get("zipCode").toString().isEmpty());
+		Assert.assertTrue(!jsonPath.get("cardType").toString().isEmpty());
+		Assert.assertTrue(!jsonPath.get("cardHolderName").toString().isEmpty());
+
+	}
+
+	@Test
+	public void getDetailsOfCardsWithInvalidAuth() throws Exception {
+		String token = DigitalWalletAPIUtils.getTokenForCustomer() + "jhgf";
+		DigitalWalletResource digitalWallet = new DigitalWalletResource();
+		Response response = digitalWallet.getDetailsOfCards(token);
+
+		Assert.assertTrue(response.getStatusCode() == 401);
+
+	}
+
 }
