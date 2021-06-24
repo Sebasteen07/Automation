6ebd17003f938792ffb57c7c9c9cf398cb6af4cc
shweta_Sontakke <shweta.sontakke@crossasyst.com>
2021-06-24 13:24:05 +0530

    PSS-18527-2 : Added Wait

--- pss/pss2patientui-objects-maps/src/main/java/com/medfusion/product/object/maps/pss2/page/Scheduled/ScheduledAppointmentAnonymous.java
+++ pss/pss2patientui-objects-maps/src/main/java/com/medfusion/product/object/maps/pss2/page/Scheduled/ScheduledAppointmentAnonymous.java
@@ -10,6 +10,7 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.How;
 import org.openqa.selenium.support.PageFactory;
 
+import com.medfusion.common.utils.IHGUtil;
 import com.medfusion.product.object.maps.pss2.page.Appointment.HomePage.HomePage;
 import com.medfusion.product.object.maps.pss2.page.Appointment.Main.PSS2MainPage;
 
@@ -35,7 +36,11 @@ public class ScheduledAppointmentAnonymous extends PSS2MainPage {
 	}
 
 	public void downloadCalander() {
+		IHGUtil.waitForElement(driver, 15, linkAddToCalander);
+		log("Before Click- ");
 		linkAddToCalander.click();
+		log("After Click");
+
 	}
 
 	public HomePage backtoHomePage() {
