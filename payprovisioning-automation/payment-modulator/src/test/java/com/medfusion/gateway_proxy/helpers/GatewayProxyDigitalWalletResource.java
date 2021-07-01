73c8abeeaf601d68cb6cec76ccb40fac1470d87f
LAPTOP-4C9D9HMK\Java <akshay.shinde@crossasyst.com>
2021-07-01 19:25:57 +0530

    resolved merge conflicts

--- payprovisioning-automation/payment-modulator/src/test/java/com/medfusion/gateway_proxy/helpers/GatewayProxyDigitalWalletResource.java
+++ payprovisioning-automation/payment-modulator/src/test/java/com/medfusion/gateway_proxy/helpers/GatewayProxyDigitalWalletResource.java
@@ -18,60 +18,36 @@ public class GatewayProxyDigitalWalletResource extends GatewayProxyBaseTest {
 
 	protected PropertyFileLoader testData;
 
-	public Response createNewWallet() throws IOException {
-		testData = new PropertyFileLoader();
-		Map<String, Object> digitalWallet = PayloadDetails.getPayloadForAddingCardToDigitalWallet(
-				testData.getProperty("consumername"), testData.getProperty("type"), testData.getProperty("cardnumber"),
-				testData.getProperty("expirationnumber"), testData.getProperty("cardalias"),
-				testData.getProperty("zipcode"));
-
-		Response response = given().spec(requestSpec).body(digitalWallet).when()
-				.post(testData.getProperty("testpaycustomeruuid") + "/wallets").then().spec(responseSpec).and()
-				.extract().response();
-		return response;
-	}
-
-	public Response getListOfCardsInWallet() throws IOException {
-
-		testData = new PropertyFileLoader();
-
-		Response response = given().that().spec(requestSpec).when()
-				.get(testData.getProperty("testpaycustomeruuid") + "/wallets/"
-						+ testData.getProperty("externalWalletId"))
-				.then().spec(responseSpec).and().extract().response();
-		return response;
-	}
-
 	public Response deleteCardInWallet(String token, String walletId, String externalcardId) throws IOException {
 
 		testData = new PropertyFileLoader();
 
 		Response response = given().spec(requestSpec).auth().oauth2(token).when().delete(
-				testData.getProperty("testpaycustomeruuid") + "/wallets/" + walletId + "/cards/" + externalcardId)
+				testData.getProperty("test.pay.customer.uuid") + "/wallets/" + walletId + "/cards/" + externalcardId)
 				.then().extract().response();
 
 		return response;
 	}
 
-	public Response createNewWallet(String token, String consumerName, String cardType, String cardNumber,
+	public Response createNewWallet(String token, String consumerName, String cardType, String cardnumber,
 			String expiryDate, String cardAlias, String zipcode) throws Exception {
 		testData = new PropertyFileLoader();
 		Map<String, Object> digitalWallet = PayloadDetails.getPayloadForAddingCardToDigitalWallet(consumerName,
-				cardType, cardNumber, expiryDate, cardAlias, zipcode);
+				cardType, cardnumber, expiryDate, cardAlias, zipcode);
 
 		Response response = given().spec(requestSpec).auth().oauth2(token).body(digitalWallet).when()
-				.post(testData.getProperty("testpaycustomeruuid") + "/wallets").then().extract().response();
+				.post(testData.getProperty("test.pay.customer.uuid") + "/wallets").then().extract().response();
 
 		return response;
 	}
 
-	public Response getListOfCardsInWallet(String token) throws IOException {
+	public Response getListOfCardsInWallet(String token, String externalWalletId) throws IOException {
 
 		testData = new PropertyFileLoader();
 
-		Response response = given().that().spec(requestSpec).auth().oauth2(token).when().get(
-				testData.getProperty("testpaycustomeruuid") + "/wallets/" + testData.getProperty("externalWalletId"))
-				.then().and().extract().response();
+		Response response = given().that().spec(requestSpec).auth().oauth2(token).when()
+				.get(testData.getProperty("test.pay.customer.uuid") + "/wallets/" + externalWalletId).then().and()
+				.extract().response();
 
 		return response;
 
@@ -88,27 +64,27 @@ public class GatewayProxyDigitalWalletResource extends GatewayProxyBaseTest {
 	}
 
 	public Response addNewCardToExistingWallet(String externalWalletId, String token, String consumerName,
-			String cardType, String cardNumber, String expiryDate, String cardAlias, String zipcode)
+			String cardType, String cardnumber, String expiryDate, String cardAlias, String zipcode)
 			throws IOException {
 		testData = new PropertyFileLoader();
 		Map<String, Object> digitalWallet = PayloadDetails.getPayloadForAddingCardToDigitalWallet(consumerName,
-				cardType, cardNumber, expiryDate, cardAlias, zipcode);
+				cardType, cardnumber, expiryDate, cardAlias, zipcode);
 
 		Response response = given().spec(requestSpec).header("Authorization", "Bearer " + token).body(digitalWallet)
-				.when().post(testData.getProperty("testpaycustomeruuid") + "/wallets/" + externalWalletId + "/cards")
+				.when().post(testData.getProperty("test.pay.customer.uuid") + "/wallets/" + externalWalletId + "/cards")
 				.then().extract().response();
 		return response;
 	}
 
-	public Response saleAPI(String accessToken, String customerUUID, String mmid, String externalWalletID, String externalCardID)
-			throws IOException {
+	public Response saleAPI(String accessToken, String customerUUID, String mmid, String externalWalletID,
+			String externalCardID) throws IOException {
 		testData = new PropertyFileLoader();
 		Map<String, Object> digitalWallet = PayloadDetails.getPayloadForNewSaleAPI(
 				testData.getProperty("payment.source"), Integer.parseInt(IHGUtil.createRandomNumericString(4)));
 
 		Response response = given()
-				.spec(requestSpec).auth().oauth2(accessToken).log().all().body(digitalWallet).when().post(customerUUID + "/merchant/" + mmid + "/wallet/"
-						+ externalWalletID + "/card/" + externalCardID + "/sale")
+				.spec(requestSpec).auth().oauth2(accessToken).log().all().body(digitalWallet).when().post(customerUUID
+						+ "/merchant/" + mmid + "/wallet/" + externalWalletID + "/card/" + externalCardID + "/sale")
 				.then().and().extract().response();
 		return response;
 	}
