431d0d93d82eedd9d53b243c691dfaba5a95b760
amruta.patil <amruta.patil@crossasyst.com>
2021-07-07 12:38:43 +0530

    Added base64 encode for auth

--- payprovisioning-automation/merchant-provisioning/src/test/java/com/medfusion/mfpay/merchant_provisioning/utils/User.java
+++ payprovisioning-automation/merchant-provisioning/src/test/java/com/medfusion/mfpay/merchant_provisioning/utils/User.java
@@ -1,19 +1,19 @@
 // Copyright 2013-2021 NXGN Management, LLC. All Rights Reserved.
 package com.medfusion.mfpay.merchant_provisioning.utils;
-import org.apache.commons.codec.binary.Base64;
+import java.util.Base64;
 
 public class User extends ProvisioningUtils{
 
     public static String getCredentialsEncodedInBase(String role) {
-        Base64 encoder = new Base64();
         if(role.equalsIgnoreCase("FINANCE")){
-        return "Basic " + encoder.encode((FINANCE_USER + ":" + FINANCE_PASS).getBytes());
+           return "Basic " + Base64.getEncoder().encodeToString((FINANCE_USER + ":" + FINANCE_PASS).getBytes());	
         }
         else if (role.equalsIgnoreCase("IMPLEMENTATION")){
-        	return "Basic " + encoder.encode((IMPLEMENTATION_USER + ":" + IMPLEMENTATION_PASS).getBytes());
+      return "Basic " + Base64.getEncoder().encodeToString((IMPLEMENTATION_USER + ":" + IMPLEMENTATION_PASS).getBytes());
+
         }
         else if(role.equalsIgnoreCase("ADMIN")){
-        	return "Basic " + encoder.encode((ADMIN_USER + ":" + ADMIN_PASS).getBytes());
+         return "Basic " + Base64.getEncoder().encodeToString((ADMIN_USER + ":" + ADMIN_PASS).getBytes());
         }
 		return null;
        
