aa651fbc6d452fd4b75c21beac9ec68a27c36f25
Giri <kypa@getnada.com>
2021-07-15 10:17:34 +0530

    Updated the properties file and SQL Query in the fucntions

--- ng-integration/src/test/java/com/ng/product/integrationplatform/tests/NGIntegrationPP174BalancePresentmentTest.java
+++ ng-integration/src/test/java/com/ng/product/integrationplatform/tests/NGIntegrationPP174BalancePresentmentTest.java
@@ -99,10 +99,10 @@ public class NGIntegrationPP174BalancePresentmentTest extends BaseTestNGWebDrive
 		String sourceId = DBUtils.executeQueryOnDB("NGCoreDB",
 				"select enc_id from patient_encounter where person_id = '" + personId + "'".trim());
 		String locationId = DBUtils.executeQueryOnDB("NGCoreDB",
-				"select locationId from location_mstr where location_name = '" + locationName + "'");
+				"select location_id from location_mstr where location_name = '" + locationName + "'");
 		String serviceitemId = propertyLoaderObj.getProperty("service.item.id");
 		String serviceitemlibId = DBUtils.executeQueryOnDB("NGCoreDB",
-				"select serviceitemlibId from service_item_mstr where serviceitemId = '" + serviceitemId + "'");
+				"select service_item_lib_Id from service_item_mstr where service_item_id = '" + serviceitemId + "'");
 		String cpt4codeId = propertyLoaderObj.getProperty("cpt4.code.id");
 		String icd9cmcodeId = propertyLoaderObj.getProperty("icd9cm.code.id");
 		String icd9cmcodeDesc = propertyLoaderObj.getProperty("icd9cm.code.desc");
@@ -201,10 +201,10 @@ public class NGIntegrationPP174BalancePresentmentTest extends BaseTestNGWebDrive
 		String sourceId = DBUtils.executeQueryOnDB("NGCoreDB",
 				"select enc_id from patient_encounter where person_id = '" + personId + "'".trim());
 		String locationId = DBUtils.executeQueryOnDB("NGCoreDB",
-				"select locationId from location_mstr where location_name = '" + locationName + "'");
+				"select location_id from location_mstr where location_name = '" + locationName + "'");
 		String serviceitemId = propertyLoaderObj.getProperty("service.item.id");
 		String serviceitemlibId = DBUtils.executeQueryOnDB("NGCoreDB",
-				"select serviceitemlibId from service_item_mstr where serviceitemId = '" + serviceitemId + "'");
+				"select service_item_lib_Id from service_item_mstr where service_item_id = '" + serviceitemId + "'");
 		String cpt4codeId = propertyLoaderObj.getProperty("cpt4.code.id");
 		String icd9cmcodeId = propertyLoaderObj.getProperty("icd9cm.code.id");
 		String icd9cmcodeDesc = propertyLoaderObj.getProperty("icd9cm.code.desc");
@@ -238,7 +238,7 @@ public class NGIntegrationPP174BalancePresentmentTest extends BaseTestNGWebDrive
 		logStep("Fetching the Test data from SQL and Properties file");
 		String serviceitemid1 = propertyLoaderObj.getProperty("service.item.id1");
 		String serviceitemlibId1 = DBUtils.executeQueryOnDB("NGCoreDB",
-				"select serviceitemlibId from service_item_mstr where serviceitemId = '" + serviceitemid1 + "'");
+				"select service_item_lib_Id from service_item_mstr where service_item_id = '" + serviceitemid1 + "'");
 		String cpt4codeId1 = propertyLoaderObj.getProperty("cpt4.code.id1");
 		String icd9cmcodeId1 = propertyLoaderObj.getProperty("cpt4.code.id1");
 		String emruniqId1 = createRandomGUID();
@@ -331,5 +331,110 @@ public class NGIntegrationPP174BalancePresentmentTest extends BaseTestNGWebDrive
 		
 
 	}
+	
+	@Test(enabled = true, groups = { "acceptance-BalancePresentment" }, retryAnalyzer = RetryAnalyzer.class)
+	public void testPatientAbletoPayExtraAmount() throws Throwable {
+		
+		String enterpriseId = propertyLoaderObj.getProperty("NGMainEnterpriseID");
+		String practiceId = propertyLoaderObj.getProperty("NGMainPracticeID");
+		String integrationPracticeId = propertyLoaderObj.getProperty("integrationpracticeid.amdc");
+		String url = propertyLoaderObj.getProperty("url");
+		String locationName = propertyLoaderObj.getProperty("EPMLocationName");
+		String providerName = propertyLoaderObj.getProperty("EPMProviderName");
+		String practiceName = propertyLoaderObj.getProperty("practiceName");
+		NGAPIUtils.updateLoginDefaultTo("EnterpriseGateway", enterpriseId, practiceId);
+		
+		logStep("Create the patient in NG EPM");		
+		NewPatient createPatient = NGPatient.patientUsingJSON(propertyLoaderObj, "complete");
+		String personId = NGAPIFlows.createPatientinEPM(createPatient);
+		NGAPIFlows.addCharttoProvider(locationName, providerName, personId);
+
+		logStep("Enroll the patient into Portal");
+		PatientEnrollment.enrollPatientWithoutGetProcessingStatusValidation(propertyLoaderObj, enterprisebaseURL,
+				ngAPIUtils, driver, createPatient, personId, practiceName, integrationPracticeId, enterpriseId,
+				practiceId);
+
+		logStep("Add Encounter to patient chart");
+		NGAPIUtils.updateLoginDefaultTo("EnterpriseGateway", enterpriseId, practiceId);
+		String encounterId = NGAPIFlows.addEncounter(locationName, providerName, personId);
+
+		logStep("Updating the Patient Encounter to be billable");
+		DBUtils.executeQueryOnDB("NGCoreDB", "update patient_encounter set billable_ind='Y' where person_id='"
+				+ personId + "' and enc_id ='" + encounterId + "'");
+
+		logStep("Fetching the values from Properties file and SQL");
+		String emruniqId = createRandomGUID();
+		String chargeId = createRandomGUID();
+		String patAmt = propertyLoaderObj.getProperty("pat.amt");
+		String amt = propertyLoaderObj.getProperty("amt");
+		String quantity = propertyLoaderObj.getProperty("quantity");
+		String sourceId = DBUtils.executeQueryOnDB("NGCoreDB",
+				"select enc_id from patient_encounter where person_id = '" + personId + "'".trim());
+		String locationId = DBUtils.executeQueryOnDB("NGCoreDB",
+				"select location_id from location_mstr where location_name = '" + locationName + "'");
+		String serviceitemId = propertyLoaderObj.getProperty("service.item.id");
+		String serviceitemlibId = DBUtils.executeQueryOnDB("NGCoreDB",
+				"select service_item_lib_Id from service_item_mstr where service_item_id = '" + serviceitemId + "'");
+		String cpt4codeId = propertyLoaderObj.getProperty("cpt4.code.id");
+		String icd9cmcodeId = propertyLoaderObj.getProperty("icd9cm.code.id");
+		String icd9cmcodeDesc = propertyLoaderObj.getProperty("icd9cm.code.desc");
+		String icd9cmcodeDesc1 = propertyLoaderObj.getProperty("icd9cm.code.desc1");
+		String renderingId = DBUtils.executeQueryOnDB("NGCoreDB",
+				"Select rendering_provider_id from patient_encounter where person_id= '" + personId + "'".trim());
+
+		logStep("Adding Charges to the Patient Encounter in NG");
+		Charges.addingChargestoPatientEncounter(enterpriseId, practiceId, emruniqId, sourceId, chargeId, personId,
+				patAmt, locationId, serviceitemlibId, serviceitemId, renderingId, cpt4codeId, amt, quantity,
+				icd9cmcodeId, icd9cmcodeDesc, icd9cmcodeDesc1);
+
+		String acctNBRQuery = "select acct_nbr from accounts where guar_id ='" + personId + "' and practice_id='"
+				+ practiceId.trim() + "'";
+
+		logStep("Login into Portal");
+		JalapenoLoginPage loginPage = new JalapenoLoginPage(driver, url);
+		JalapenoHomePage homePage = loginPage.login(createPatient.getEmailAddress(), propertyLoaderObj.getPassword());
+		
+		logStep("Wait 120 second so that added charges in NG will be synced with Portal");
+		Thread.sleep(120000);
+		logStep("Refreshing the page so that charges will be displayed in Portal");
+		driver.navigate().refresh();
+		driver.findElement(By.xpath("//span[@class='badge amountDue']")).isDisplayed();
+		JalapenoPayBillsMakePaymentPage payBillsPage = homePage.clickOnNewPayBills(driver);
+
+		logStep("Verifying the expected and actual bill amount");
+		String AmountText = driver.findElement(By.xpath("//div[@id='balanceDue']//strong")).getText().substring(1);
+
+		logStep("Amount which is displayed in Portal:" + AmountText);
+		CommonUtils.VerifyTwoValues(AmountText, "equals", propertyLoaderObj.getProperty("pat.amt"));
+		logStep("Remove all cards because Selenium can't see AddNewCard button");
+		payBillsPage.removeAllCards();
+		logStep("Check that no card is present");
+		assertFalse(payBillsPage.isAnyCardPresent());
+		logStep("Initiate payment data");
+		String accountNumber = DBUtils.executeQueryOnDB("NGCoreDB", acctNBRQuery);
+		String extrapatamt = propertyLoaderObj.getProperty("pat.extra.amt");
+		String name = "TestPatient CreditCard";
+		CreditCard creditCard = new CreditCard(CardType.Discover, name);
+		JalapenoPayBillsConfirmationPage confirmationPage = payBillsPage.fillPaymentInfo(extrapatamt, accountNumber,
+				creditCard);
+		logStep("Verifying credit card ending");
+		assertTrue(confirmationPage.getCreditCardEnding().equals(creditCard.getLastFourDigits()));
+		String paymentComments = "Testing payment from number: " + accountNumber + " Current Timestamp "
+				+ (new Date()).getTime();
+		homePage = confirmationPage.commentAndSubmitPayment(paymentComments);
+		assertTrue(homePage.wasPayBillsSuccessfull());
+		homePage.clickOnLogout();
+		logStep("Verifying the patient paid extra amount is reached to NG");
+		CommonFlows.verifyPaymentReachedtoMFAgent(paymentComments, "BillPayment", "", accountNumber, "POSTED", extrapatamt,
+				"Discover");
+		String sourceIdQuery = DBUtils.executeQueryOnDB("NGCoreDB", "select acct_id from accounts where guar_id ='"
+				+ personId + "' and practice_id='" + practiceId.trim() + "'");
+		CommonFlows.verifyPaymentPostedtoNG(paymentComments, sourceIdQuery.toUpperCase(), personId.toUpperCase(),
+				"-" + extrapatamt, "Payment type: BillPayment, Last 4 CC digits: " + creditCard.getLastFourDigits(),
+				practiceId);
+	
+		
+	}
+	
 
 }
