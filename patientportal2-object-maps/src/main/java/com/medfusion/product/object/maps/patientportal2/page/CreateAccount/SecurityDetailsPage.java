4780d3a28acf47692223dbfd4e4d4ceb7cd2b22a
HEALTH\shouryan <shourya.ns@healthasyst.com>
2021-08-03 12:14:06 +0530

    Automation : Medication Request for Dependent Account with fee

--- patientportal2-object-maps/src/main/java/com/medfusion/product/object/maps/patientportal2/page/CreateAccount/SecurityDetailsPage.java
+++ patientportal2-object-maps/src/main/java/com/medfusion/product/object/maps/patientportal2/page/CreateAccount/SecurityDetailsPage.java
@@ -87,7 +87,7 @@ public class SecurityDetailsPage extends MedfusionPage {
 	}
 
 		public JalapenoHomePage fillAccountDetailsAndContinue(String userId, String password, PropertyFileLoader testData) throws InterruptedException {
-				return fillAccountDetailsAndContinue(userId, password, testData.getSecretQuestion(), testData.getSecretAnswer(), testData.getPhoneNumber(), 2);
+			return fillAccountDetailsAndContinue(userId, password, testData.getProperty("secret.question"), testData.getProperty("secret.answer"), testData.getProperty("phone.number"), 2);
 		}
 
 		public JalapenoHomePage fillAccountDetailsAndContinue(String userId, String password, String secretQuestion, String secretAnswer, String phoneNumber) throws InterruptedException {
