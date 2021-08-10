3c692d7ff6f11499c4179b93fb35164c1d620c71
HEALTH\shouryan <shourya.ns@healthasyst.com>
2021-08-10 19:10:47 +0530

    Addressed review comments

--- jalapeno-automation/src/test/java/com/medfusion/patientportal2/test/PatientPortal2AcceptanceTests.java
+++ jalapeno-automation/src/test/java/com/medfusion/patientportal2/test/PatientPortal2AcceptanceTests.java
@@ -4821,7 +4821,7 @@ public class PatientPortal2AcceptanceTests extends BaseTestNGWebDriver {
 		appointmentRequestStep2.fillAppointmentRequestForm(appointmentReason);
 		homePage = appointmentRequestStep2.submitAppointment(driver);
 
-		logStep("Check if thank you frame is displayd");
+		logStep("Check if thank you frame is displayed");
 		Thread.sleep(10000);
 		assertTrue(homePage.isTextDisplayed("Thank you"));
 
@@ -4896,18 +4896,17 @@ public class PatientPortal2AcceptanceTests extends BaseTestNGWebDriver {
 
 		JalapenoAppointmentRequestV2Step1 appointmentRequestStep1 = PageFactory.initElements(driver,
 				JalapenoAppointmentRequestV2Step1.class);
-		logStep("Assess Elements and choose provider");
+		logStep("Choose a provider");
 		appointmentRequestStep1.chooseFirstProvider();
 
-		logStep("Continue to step 2.: click continue and assess elements");
+		logStep("Continue to step 2.: click continue");
 		JalapenoAppointmentRequestV2Step2 appointmentRequestStep2 = appointmentRequestStep1.continueToStep2(driver);
 
 		logStep("Fill details and submit");
 		appointmentRequestStep2.fillAppointmentRequestForm(appointmentReason);
 		homePage = appointmentRequestStep2.submitAppointment(driver);
 
-		logStep("Check if thank you frame is displayd");
-		Thread.sleep(10000);
+		logStep("Check if thank you frame is displayed");
 		assertTrue(homePage.isTextDisplayed("Thank you"));
 
 		logStep("Navigate to Appointment Request History");
@@ -4922,7 +4921,7 @@ public class PatientPortal2AcceptanceTests extends BaseTestNGWebDriver {
 		assertTrue(historyPage.checkAppointmentDetails(appointmentReason));
 		homePage.clickOnLogout();
 
-		logStep("Proceed in Practice Portal to Approved the request with adding Attachment");
+		logStep("Proceed in Practice Portal to Approve the request with adding Attachment");
 		AppoitmentRequest practicePortal = new AppoitmentRequest();
 		long tsPracticePortal = practicePortal.ProceedAppoitmentRequesAttachmentt(driver, true, appointmentReason,
 				testData.getPortalUrl(), testData.getDoctorLogin2(), testData.getDoctorPassword(), MessageErrorfilePath,
