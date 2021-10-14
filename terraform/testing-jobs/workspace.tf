6605ec055119c841d065cf48b522108aa0e58d02
trasad <tprasad@nextgen.com>
2021-10-14 10:44:18 +0530

    BAR-16020 including the patientportal group of jobs

--- terraform/testing-jobs/workspace.tf
+++ terraform/testing-jobs/workspace.tf
@@ -5,6 +5,10 @@ locals {
     git-taf-prod-mu2-accessibility = "git-taf-prod-mu2-accessibility"
     git-taf-prod-mu2-regression = "git-taf-prod-mu2-regression"
     git-taf-prod-mu2-acceptance = "git-taf-prod-mu2-acceptance"
+    prod-patientportal-regression1 = "prod-patientportal-regression1"
+    prod-patientportal-regression2 = "prod-patientportal-regression2"
+    prod-patientportal-regression3 = "prod-patientportal-regression3"
+    prod-patientportal-regression4 = "prod-patientportal-regression4"
   }
   selected_workspace = local.valid_workspaces[terraform.workspace]
 }
