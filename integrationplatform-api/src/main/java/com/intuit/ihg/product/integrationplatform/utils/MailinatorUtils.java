9e7ab9df31baba44fceb219d36549d62d7d530ed
sunilxavier93 <sunil.xavier@crossasyst>
2021-12-20 14:17:50 +0530

    PMEMR-5658 updated code for mailinator UI changes

--- integrationplatform-api/src/main/java/com/intuit/ihg/product/integrationplatform/utils/MailinatorUtils.java
+++ integrationplatform-api/src/main/java/com/intuit/ihg/product/integrationplatform/utils/MailinatorUtils.java
@@ -9,11 +9,14 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.How;
 
+import com.intuit.ifs.csscat.core.utils.Log4jUtil;
 import com.medfusion.product.object.maps.patientportal2.page.MedfusionPage;
 
 public class MailinatorUtils extends MedfusionPage{
 	
  	private static final String MAILINATOR_INBOX_TEMPLATE_URL = "https://www.mailinator.com/v4/public/inboxes.jsp?to=";
+
+	private static final int TIME_TO_WAIT_MS = 10000;
 
 	@FindBy(how = How.XPATH, using = "(//tr[@ng-repeat='email in emails'])[1]/td[3]")
 	private WebElement firstRowSubject;
@@ -34,31 +37,75 @@ public class MailinatorUtils extends MedfusionPage{
 	    super(driver);
 	}
 
-	public String getLinkFromEmail(String username, String emailSubject, String findInEmail, int retries) {
-	    String link = "";
+	public String getLinkFromEmail(String username, String emailSubject, String findInEmail, int retries)
+		throws InterruptedException {
 	    this.driver.get(MAILINATOR_INBOX_TEMPLATE_URL + username);
-	    try {
-		if (this.isTextVisible(emailSubject)) {
-		    this.clickOnElement(firstRowSubject);
-		    this.driver.switchTo().frame(iframe);
+	    for (int j = 1; j <= retries; j++) {
+		try {
+		    if (this.isTextVisible(emailSubject)) {
+			this.clickOnElement(firstRowSubject);
+			this.driver.switchTo().frame(iframe);
 
-		    String parentWindow = driver.getWindowHandle();
-		    driver.findElement(By.xpath("//a[contains(text(),'" + findInEmail + "')]")).click();
+			String parentWindow = driver.getWindowHandle();
+			driver.findElement(By.xpath("//a[contains(text(),'" + findInEmail + "')]")).click();
 
-		    Set<String> handles = driver.getWindowHandles();
-		    for (String windowHandle : handles) {
-			if (windowHandle.equals(parentWindow)) {
-			    driver.close(); // closing parent window
-			} else {
-			    driver.switchTo().window(windowHandle);
+			Set<String> handles = driver.getWindowHandles();
+			for (String windowHandle : handles) {
+			    if (handles.size() > 1 && windowHandle.equals(parentWindow)) {
+				driver.close(); // closing parent window
+			    } else {
+				driver.switchTo().window(windowHandle);
+			    }
 			}
+			return this.driver.getCurrentUrl();
 		    }
-		    link = this.driver.getCurrentUrl();
+		} catch (Exception e) {
+		    log("Exception found: " + e.getMessage());
 		}
-	    } catch (Exception e) {
-		log("Exception found: " + e.getMessage());
+		logAttemptAndSleep(j, retries);
 	    }
-	    return link;
+
+	    return null;
 	}
 
+	public boolean isMessageInInbox(String username, String emailSubject, String findInEmail, int retries)
+		throws Exception {
+
+	    for (int j = 1; j <= retries; j++) {
+		this.driver.get(MAILINATOR_INBOX_TEMPLATE_URL + username);
+		try {
+		    if (this.isTextVisible(emailSubject)) {
+			this.clickOnElement(firstRowSubject);
+			this.driver.switchTo().frame(iframe);
+
+			if (driver.findElement(By.xpath("//a[contains(text(),'" + findInEmail + "')]")).isDisplayed()) {
+			    return true;
+
+			}
+		    }
+		} catch (Exception e) {
+		    log("Exception found: " + e.getMessage());
+		}
+		logAttemptAndSleep(j, retries);
+	    }
+	    return false;
+	}
+
+	private void logAttemptAndSleep(int currentAttempt, int maxAttempts) throws InterruptedException {
+	    logAttemptAndSleep(currentAttempt, maxAttempts, false);
+	}
+
+	private void logAttemptAndSleep(int currentAttempt, int maxAttempts, boolean plural)
+		throws InterruptedException {
+	    Log4jUtil.log((plural ? "Emails were" : "Email was") + " not retrieved. Trial number " + currentAttempt
+		    + "/" + maxAttempts + "."
+		    + (currentAttempt != maxAttempts
+			    ? " Waiting for e-mail" + (plural ? "s" : "") + " to arrive for " + TIME_TO_WAIT_MS / 1000
+				    + " s."
+			    : ""));
+	    // we don't need to sleep after the last retry
+	    if (currentAttempt != maxAttempts) {
+		Thread.sleep(TIME_TO_WAIT_MS);
+	    }
+	}
 }
