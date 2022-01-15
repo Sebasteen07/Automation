3981f233019b7742c86a305cfc43f3d070f310cc
ShwetaSontakke <shweta.sontakke@crossasyst.com>
2022-01-15 18:16:50 +0530

    PSS-20113

---
+++ pss/pss2patientui-automation/src/test/java/com/medfusion/product/pss2patientmodulatorapi/test/PSS2PMFeatureNGTests05.java
@@ -0,0 +1,220 @@
+// Copyright 2022 NXGN Management, LLC. All Rights Reserved.
+package com.medfusion.product.pss2patientmodulatorapi.test;
+
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertTrue;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import com.intuit.ifs.csscat.core.BaseTestNG;
+import com.intuit.ifs.csscat.core.RetryAnalyzer;
+import com.medfusion.product.object.maps.pss2.page.util.APIVerification;
+import com.medfusion.product.object.maps.pss2.page.util.HeaderConfig;
+import com.medfusion.product.object.maps.pss2.page.util.PostAPIRequestAdapterModulator;
+import com.medfusion.product.object.maps.pss2.page.util.PostAPIRequestPMNG;
+import com.medfusion.product.pss2patientapi.payload.PayloadAM02;
+import com.medfusion.product.pss2patientapi.payload.PayloadAdapterModulator;
+import com.medfusion.product.pss2patientapi.payload.PayloadPM02;
+import com.medfusion.product.pss2patientapi.payload.PayloadPssPMNG;
+import com.medfusion.product.pss2patientui.pojo.AdminUser;
+import com.medfusion.product.pss2patientui.pojo.Appointment;
+import com.medfusion.product.pss2patientui.utils.PSSPatientUtils;
+import com.medfusion.product.pss2patientui.utils.PSSPropertyFileLoader;
+
+import io.restassured.response.Response;
+
+public class PSS2PMFeatureNGTests05 extends BaseTestNG {
+
+	public static HeaderConfig headerConfig;
+	public static PSSPropertyFileLoader propertyData;
+
+	public static Appointment testData;
+	public static PostAPIRequestPMNG postAPIRequest;
+	public static PayloadPssPMNG payloadPatientMod;
+	public static PayloadPM02 payloadPM02;
+	public static String accessToken;
+	public static String practiceid;
+	public static String baseurl;
+	public PSSPatientUtils pssPatientUtils;
+	public PSS2PatientModulatorrAcceptanceNGTests pmng;
+	
+	public static PayloadAdapterModulator payloadAM;
+	public static PayloadAM02 payloadAM02;
+	public static PostAPIRequestAdapterModulator postAPIRequestAM;
+	public static String openToken;
+	public static String practiceId;
+
+	APIVerification apv;
+
+	@BeforeTest(enabled = true, groups = { "APItest" })
+	public void setUpPM() throws IOException {
+
+		log("Set the Authorization for Patient Modulator");
+
+		headerConfig = new HeaderConfig();
+		propertyData = new PSSPropertyFileLoader();
+		testData = new Appointment();
+		propertyData.setRestAPIDataPatientModulator(testData);
+		postAPIRequest = new PostAPIRequestPMNG();
+		payloadPatientMod = new PayloadPssPMNG();
+
+		payloadPM02 = new PayloadPM02();
+
+		baseurl = propertyData.getProperty("base.url.pm.ng");
+		practiceid = propertyData.getProperty("mf.practice.id.ng");
+		accessToken = postAPIRequest.createToken(baseurl, practiceid);
+		pssPatientUtils = new PSSPatientUtils();
+		apv = new APIVerification();
+		pmng = new PSS2PatientModulatorrAcceptanceNGTests();
+		log("Base URL for Patient Modulator - " + baseurl);
+	}
+	
+	public void setUpAM(String practiceId1, String userID) throws IOException {
+		payloadAM02 = new PayloadAM02();
+		payloadAM = new PayloadAdapterModulator();
+		propertyData = new PSSPropertyFileLoader();
+		postAPIRequestAM = new PostAPIRequestAdapterModulator();
+		headerConfig = new HeaderConfig();
+		practiceId = practiceId1;
+		log("BASE URL AM -" + propertyData.getProperty("base.url.am"));
+		openToken = postAPIRequestAM.openToken(propertyData.getProperty("base.url.am"), practiceId,
+				payloadAM02.openTokenPayload(practiceId, userID));
+		postAPIRequestAM.setupRequestSpecBuilder(propertyData.getProperty("base.url.am"),
+				headerConfig.HeaderwithToken(openToken));
+	}
+
+	@Test(enabled = true, groups = { "APItest" }, retryAnalyzer = RetryAnalyzer.class)
+	public void test01AnnouncementByLanguageGET() throws IOException {
+		
+		logStep("Set up the API authentication");
+		setUpAM(propertyData.getProperty("mf.practice.id.ng"), propertyData.getProperty("mf.authuserid.am.ng"));
+
+		Response response;
+
+		response = postAPIRequest.announcementByLanguage(baseurl, headerConfig.HeaderwithToken(accessToken),
+				practiceid);
+		apv.responseCodeValidation(response, 200);
+		apv.responseTimeValidation(response);
+	}
+	
+	@Test(enabled = true, groups = { "APItest" }, retryAnalyzer = RetryAnalyzer.class)
+	public void testPatientStatusInactiveAlert_Spanish() throws Exception {
+
+		logStep("Set up the API authentication");
+		setUpAM(propertyData.getProperty("mf.practice.id.ng"), propertyData.getProperty("mf.authuserid.am.ng"));
+
+		Response response;
+		String adminPayload;
+
+		String patientId = propertyData.getProperty("patient.id.inactive.pm.ng");
+		String patientStatus = propertyData.getProperty("patientstatus.id.ng");
+
+		adminPayload = payloadAM02.inactivePatientPyaload(Integer.parseInt(patientStatus));
+
+		response = postAPIRequestAM.lockoutPost(practiceId, adminPayload, "/lockout");
+		apv.responseCodeValidation(response, 200);
+		apv.responseTimeValidation(response);
+
+		response = postAPIRequest.patientDemographics(baseurl, headerConfig.HeaderwithTokenES(accessToken), practiceId,
+				patientId);
+		apv.responseCodeValidation(response, 200);
+		apv.responseTimeValidation(response);
+
+		String showAlerts = apv.responseKeyValidationJson(response, "showAlert");
+
+		boolean showAlertsValue = Boolean.parseBoolean(showAlerts);
+		assertEquals(showAlertsValue, true, "Alert is not getting displayed so test case failed");
+	}
+	
+	@Test(enabled = true, groups = { "APItest" }, retryAnalyzer = RetryAnalyzer.class)
+	public void testApptStackingOverbooking() throws Exception {
+
+		logStep("Set up the API authentication");
+		setUpAM(propertyData.getProperty("mf.practice.id.ng"), propertyData.getProperty("mf.authuserid.am.ng"));
+
+		Response response;
+		JSONArray arr;
+		String adminPayload;
+		
+		adminPayload=payloadAM02.overbookingPyaload(true);
+		response=postAPIRequestAM.bookAppointmentTypeUpdate(practiceId, adminPayload);
+		apv.responseCodeValidation(response, 200);
+		apv.responseTimeValidation(response);
+		
+		String patientId01 = propertyData.getProperty("patient.id.pm.ng");
+		String patientId02 = propertyData.getProperty("patient.id.overbooking.pm.ng");
+
+		String currentdate = pssPatientUtils.sampleDateTime("MM/dd/yyyy");
+
+		String b = payloadPatientMod.availableslotsPayload(currentdate,
+				propertyData.getProperty("availableslot.bookid.pm.ng03"),
+				propertyData.getProperty("availableslot.locationid.pm.ng"),
+				propertyData.getProperty("availableslot.apptid.pm.ng"));
+
+		response = postAPIRequest.availableSlots(baseurl, b, headerConfig.HeaderwithToken(accessToken),
+				practiceid, patientId01);
+		apv.responseCodeValidation(response, 200);
+		apv.responseTimeValidation(response);
+
+		arr= new JSONArray(response.body().asString());
+
+		String slotid = arr.getJSONObject(0).getJSONArray("slotList").getJSONObject(0).getString("slotId");
+		String time = arr.getJSONObject(0).getJSONArray("slotList").getJSONObject(0).getString("slotTime");
+		String date = arr.getJSONObject(0).getString("date");
+
+		log("slotTime-" + time);
+		log("slotId- " + slotid);
+		log("Date-" + date);
+
+		String c = payloadPatientMod.scheduleAppointmentPayload(slotid, date, time,
+				propertyData.getProperty("availableslot.bookid.pm.ng03"),
+				propertyData.getProperty("availableslot.locationid.pm.ng"),
+				propertyData.getProperty("availableslot.apptid.pm.ng"));
+
+		log("Payload-" + c);
+
+		Response schedResponse = postAPIRequest.scheduleAppointment(baseurl, c,
+				headerConfig.HeaderwithToken(accessToken), practiceid, patientId01, testData.getPatientType());
+		apv.responseCodeValidation(schedResponse, 200);
+		apv.responseTimeValidation(schedResponse);
+
+		apv.responseKeyValidationJson(schedResponse, "confirmationNo");
+		apv.responseKeyValidationJson(schedResponse, "patientId");
+		String extApptId = apv.responseKeyValidationJson(schedResponse, "extApptId");
+		
+		response = postAPIRequest.availableSlots(baseurl, b, headerConfig.HeaderwithToken(accessToken),
+				practiceid, patientId02);
+		apv.responseCodeValidation(response, 200);
+		apv.responseTimeValidation(response);
+
+		arr = new JSONArray(response.body().asString());
+
+		String time2 = arr.getJSONObject(0).getJSONArray("slotList").getJSONObject(0).getString("slotTime");
+		String date2 = arr.getJSONObject(0).getString("date");
+
+		log("slotTime-" + time);
+		log("slotId- " + slotid);
+		log("Date-" + date);
+		
+		assertEquals(date2, date);
+		assertEquals(time2, time);	
+
+		Response cancelResponse = postAPIRequest.cancelAppointment(baseurl,
+				payloadPatientMod.cancelAppointmentPayload(extApptId), headerConfig.HeaderwithToken(accessToken),
+				practiceid, patientId01);
+		apv.responseCodeValidation(cancelResponse, 200);
+		
+		adminPayload=payloadAM02.overbookingPyaload(true);
+		response=postAPIRequestAM.bookAppointmentTypeUpdate(practiceId, adminPayload);
+		apv.responseCodeValidation(response, 200);
+
+	}
+	
+	
+}
