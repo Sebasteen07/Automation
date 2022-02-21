72985b76885f180dfb5cb05a005eabb997cd7e9c
LAPTOP-OK48I1C9\Java <sujit.kolhe@crossasyst.com>
2022-02-21 11:15:43 +0530

    Added Commom method in appointment Api

--- appointment-precheckui-api/src/main/java/com/medfusion/product/object/maps/appt/precheck/util/CommonMethods.java
+++ appointment-precheckui-api/src/main/java/com/medfusion/product/object/maps/appt/precheck/util/CommonMethods.java
@@ -1,11 +1,25 @@
 // Copyright 2013-2021 NXGN Management, LLC. All Rights Reserved.
 package com.medfusion.product.object.maps.appt.precheck.util;
 
+import java.util.Calendar;
 import java.util.Random;
-import org.openqa.selenium.WebDriver;
+
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebElement;
+
 import com.intuit.ifs.csscat.core.BaseTestNGWebDriver;
 
-public class CommonMethods {
+public class CommonMethods extends BaseTestNGWebDriver{
+	JavascriptExecutor jse;
+	
+	public void highlightElement(WebElement element) {
+		jse = (JavascriptExecutor) driver;
+		jse.executeScript("arguments[0].setAttribute('style','border: solid 6px red');", element);
+	}
+
+	public String getDate(Calendar cal) {
+		return "" + cal.get(Calendar.DATE) + "-" + (cal.get(Calendar.MONTH) + 1) + "-" + cal.get(Calendar.YEAR);
+	}
 
 	public String generateRandomNum() {
 		Random random = new Random();
