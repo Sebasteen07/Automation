d768780c2d6a74c88cb73ba94e7f59d3a769b587
DESKTOP-3MF69ND\CA-NarmadaS <narmadha.sivakumar@crossasyst.com>
2022-07-20 11:35:53 -0700

    Changes

--- appointment-precheckui-api/src/main/java/com/medfusion/product/object/maps/appt/precheck/util/APIVerification.java
+++ appointment-precheckui-api/src/main/java/com/medfusion/product/object/maps/appt/precheck/util/APIVerification.java
@@ -81,14 +81,14 @@ public class APIVerification extends BaseTestNGWebDriver {
 		long time = response.time();
 		log("Response time " + time);
 		ValidatableResponse valRes = response.then();
-		valRes.time(Matchers.greaterThan(100L));
+		valRes.time(Matchers.greaterThan(0L));
 	}
 
 	public void responseTimeValidationDailyAggregation(Response response) {
 		long time = response.time();
 		log("Response time " + time);
 		ValidatableResponse valRes = response.then();
-		valRes.time(Matchers.greaterThan(100L));
+		valRes.time(Matchers.greaterThan(0L));
 	}
 
 	public void verifyInvalidLanguage(Response response) throws IOException {
